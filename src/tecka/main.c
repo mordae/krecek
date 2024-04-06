7b5b4b537ae826713aee2471e887410e759ba785
Jan Hamal Dvořák <mordae@anilinux.org>
2024-04-06 15:47:55 +0200

    Port tecka

--- src/tecka/main.c
+++ src/tecka/main.c
@@ -28,13 +28,12 @@
 #include <hardware/regs/resets.h>
 #include <hardware/regs/xosc.h>
 
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <dap.h>
 #include <task.h>
 #include <tft.h>
-#include <dap.h>
 
 #define DAP_SWDIO_PIN 25
 #define DAP_SWCLK_PIN 24
@@ -58,83 +57,14 @@
 #define SLAVE_START_PIN 19
 #define SLAVE_SELECT_PIN 20
 
-static bool p1_up_btn = 0;
-static bool p1_gun_btn = 0;
-
-static bool p2_up_btn = 0;
-static bool p2_gun_btn = 0;
+static int input_joy_x = 0;
+static int input_joy_y = 0;
 
 static float bat_mv = 0.0;
 
 static void stats_task(void);
 static void tft_task(void);
 static void input_task(void);
-
-struct hamster {
-	float y;
-	float dy;
-	uint8_t color;
-	float px, py;
-	int hp;
-};
-
-static struct hamster p1, p2;
-
-uint32_t heart_sprite[32] = {
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00011100011100000000000000000000, /* do not wrap please */
-	0b00111110111110000000000000000000, /* do not wrap please */
-	0b01111111111111000000000000000000, /* do not wrap please */
-	0b01111111111111000000000000000000, /* do not wrap please */
-	0b01111111111111000000000000000000, /* do not wrap please */
-	0b01111111111111000000000000000000, /* do not wrap please */
-	0b00111111111110000000000000000000, /* do not wrap please */
-	0b00011111111100000000000000000000, /* do not wrap please */
-	0b00001111111000000000000000000000, /* do not wrap please */
-	0b00000111110000000000000000000000, /* do not wrap please */
-	0b00000011100000000000000000000000, /* do not wrap please */
-	0b00000001000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-	0b00000000000000000000000000000000, /* do not wrap please */
-};
-
-static void draw_sprite(int x0, int y0, uint32_t sprite[32], int color, bool transp)
-{
-	int x1 = x0 + 32;
-	int y1 = y0 + 32;
-
-	for (int y = y0; y < y1; y++) {
-		for (int x = x0; x < x1; x++) {
-			bool visible = (sprite[(y - y0)] << (x - x0)) >> 31;
-
-			if (!visible && transp)
-				continue;
-
-			int c = color * visible;
-			tft_draw_pixel(x, y, c);
-		}
-	}
-}
-
-#define WIDTH 160
-#define HEIGHT 120
 
 /*
  * Tasks to run concurrently:
@@ -208,23 +138,15 @@ static void input_task(void)
 	task_sleep_ms(300);
 
 	while (true) {
-		p1_up_btn = !slave_gpio_get(SLAVE_A_PIN);
-		p1_gun_btn = !slave_gpio_get(SLAVE_B_PIN);
-
-		p2_up_btn = !slave_gpio_get(SLAVE_X_PIN);
-		p2_gun_btn = !slave_gpio_get(SLAVE_Y_PIN);
-
 		if (!slave_gpio_get(SLAVE_SELECT_PIN)) {
 			puts("SELECT");
 			dap_poke(0x40018004, 0x331f);
 		}
 
-#if 0
-		int joy_x = 4095 - slave_adc_read(2);
-		int joy_y = 4095 - slave_adc_read(3);
+		input_joy_x = 2048 - slave_adc_read(2) - 197;
+		input_joy_y = 2048 - slave_adc_read(3) - 172;
 
-		printf("joy: %4i  %4i\n", joy_x, joy_y);
-#endif
+		//printf("joy: x=%5i, y=%5i\n", input_joy_x, input_joy_y);
 
 		adc_select_input(2);
 		int bat = 0;
@@ -243,7 +165,7 @@ static void input_task(void)
 	}
 }
 
-inline __unused static int clamp(int x, int lo, int hi)
+inline static int clamp(int x, int lo, int hi)
 {
 	if (x < lo)
 		return lo;
@@ -254,203 +176,77 @@ inline __unused static int clamp(int x, int lo, int hi)
 	return x;
 }
 
-static void reset_game(void)
-{
-	p1.color = RED;
-	p1.dy = 0;
-	p1.y = tft_height - 31;
-	p1.px = -1;
-	p1.py = -1;
-	p1.hp = 3;
-
-	p2.color = GREEN;
-	p2.dy = 0;
-	p2.y = tft_height - 31;
-	p2.px = -1;
-	p2.py = -1;
-	p2.hp = 3;
-}
-
 /*
  * Outputs stuff to the screen as fast as possible.
  */
 static void tft_task(void)
 {
 	uint32_t last_sync = time_us_32();
-	int fps = 30;
+	int fps = 0;
 
-	reset_game();
+	int ew = 30;
+	int eh = 30;
+	int ex = random() % (tft_width - ew);
+	int ey = random() % (tft_height - eh);
+	int ehp_max = 59;
+	int ehp = ehp_max;
+	int exa = 2;
+	int eya = 2;
+	int score = 0;
 
 	while (true) {
 		tft_fill(0);
-
-		float bottom = tft_height - 31;
-
-		/*
-		 * Draw hamsters
-		 */
-
-		tft_draw_rect(0, p1.y, 23, p1.y + 31, p1.color);
-		tft_draw_rect(tft_width - 24, p2.y, tft_width - 1, p2.y + 31, p2.color);
-
-		/*
-		 * Draw hearts
-		 */
-
-		for (int i = 0; i < p1.hp; i++)
-			draw_sprite(28 + 16 * i, 4, heart_sprite, RED, true);
-
-		for (int i = 0; i < p2.hp; i++)
-			draw_sprite(tft_width - 17 - (28 + 16 * i), 4, heart_sprite, GREEN, true);
-
-		/*
-		 * Jumping
-		 */
-
-		if ((p1.y >= tft_height - 31) && p1_up_btn)
-			p1.dy = -tft_height * 1.15;
-
-		if ((p1.px < 0) && p1_gun_btn) {
-			p1.px = 24;
-			p1.py = p1.y + 16;
-		}
-
-		if ((p2.y >= tft_height - 31) && p2_up_btn)
-			p2.dy = -tft_height * 1.15;
-
-		if ((p2.px < 0) && p2_gun_btn) {
-			p2.px = tft_width - 25;
-			p2.py = p2.y + 16;
-		}
-
-		/*
-		 * Vertical movement
-		 */
-
-		p1.y += p1.dy / fps;
-		p2.y += p2.dy / fps;
-
-		/*
-		 * Gravitation
-		 */
-
-		p1.dy += (float)tft_height / fps;
-		p2.dy += (float)tft_height / fps;
-
-		/*
-		 * Fall boosting
-		 */
-
-		if (p1.dy > 0 && p1_up_btn) {
-			p1.dy += (float)tft_height / fps;
-		}
-
-		if (p2.dy > 0 && p2_up_btn) {
-			p2.dy += (float)tft_height / fps;
-		}
-
-		/*
-		 * Cap acceleration and keep hamsters above floor
-		 */
-
-		if (p1.dy > tft_height)
-			p1.dy = tft_height;
-
-		if (p2.dy > tft_height)
-			p2.dy = tft_height;
-
-		if (p1.y >= bottom)
-			p1.y = bottom;
-
-		if (p2.y >= bottom)
-			p2.y = bottom;
-
-		/*
-		 * Draw projectiles
-		 */
-
-		if (p1.px >= 0)
-			tft_draw_rect(p1.px - 1, p1.py - 1, p1.px + 1, p1.py + 1, p1.color);
-
-		if (p2.px >= 0)
-			tft_draw_rect(p2.px - 1, p2.py - 1, p2.px + 1, p2.py + 1, p2.color);
-
-		/*
-		 * Mid-air projectile collissions
-		 */
-
-		if (p1.px >= 0 && p2.px >= 0) {
-			if ((p1.py <= p2.py + 1) && (p1.py >= p2.py - 1)) {
-				/* Projectiles are at about the same height. */
-
-				if (p1.px >= p2.px) {
-					/* They must have collided. */
-					p1.px = -1;
-					p2.px = -1;
-				}
-			}
-		}
-
-		/*
-		 * Horizontal projectile movement
-		 */
-
-		float pdistance = 0.5 * (float)tft_width / fps;
-
-		if (p1.px >= 0)
-			p1.px += pdistance;
-
-		if (p2.px >= 0)
-			p2.px -= pdistance;
-
-		if (p1.px >= tft_width)
-			p1.px = -1;
-
-		if (p2.px < 0)
-			p2.px = -1;
-
-		/*
-		 * Projectile-hamster collissions
-		 */
-
-		if (p1.px >= 0) {
-			if (p1.py >= p2.y && p1.py < (p2.y + 32)) {
-				if (p1.px >= tft_width - 24) {
-					p1.px = -1;
-					p2.hp -= 1;
-
-					if (p2.hp < 1)
-						reset_game();
-				}
-			}
-		}
-
-		if (p2.px >= 0) {
-			if (p2.py >= p1.y && p2.py < (p1.y + 32)) {
-				if (p2.px < 24) {
-					p2.px = -1;
-					p1.hp -= 1;
-
-					if (p1.hp < 1)
-						reset_game();
-				}
-			}
-		}
-
-		/*
-		 * FPS and others
-		 */
-
 		char buf[64];
+
+		snprintf(buf, sizeof buf, "%i", score);
+		tft_draw_string(0, 0, RED, buf);
+
+		/* Calculate crosshair position */
+		int tx = tft_width / 2 * input_joy_x / 1200;
+		int ty = tft_height / 2 * input_joy_y / 1200;
+
+		tx += tft_width / 2;
+		ty += tft_height / 2;
+
+		tx = clamp(tx, 0, tft_width - 1);
+		ty = clamp(ty, 0, tft_height - 1);
+
+		/* Check for collission */
+		if ((tx >= ex) && (tx <= (ex + ew)) && (ty >= ey) && (ty <= (ey + eh))) {
+			ehp = MAX(0, ehp - 1);
+		} else {
+			ehp = MIN(ehp_max, ehp + 1);
+		}
+
+		if (!ehp) {
+			ex = random() % (tft_width - ew);
+			ey = random() % (tft_height - eh);
+			ehp = ehp_max;
+			ew = clamp(ew - 2, 10, 30);
+			eh = clamp(eh - 2, 10, 30);
+			score++;
+		} else {
+			ex += exa * (random() % 3 - 1);
+			ey += eya * (random() % 3 - 1);
+
+			ex = clamp(ex, 0, tft_width - ew);
+			ey = clamp(ey, 0, tft_height - eh);
+		}
+
+		int ecolor = 249 + 6 - ehp / 10;
+
+		/* draw THE enemy */
+		tft_draw_rect(ex, ey, ex + ew, ey + eh, ecolor);
+
+		/* Draw the crosshair */
+		tft_draw_rect(tx - 2, ty, tx + 2, ty, GREEN);
+		tft_draw_rect(tx, ty - 2, tx, ty + 2, GREEN);
 
 		snprintf(buf, sizeof buf, "%i", fps);
 		tft_draw_string_right(tft_width - 1, 0, GRAY, buf);
 
-		snprintf(buf, sizeof buf, "%.1f mV", bat_mv);
-		tft_draw_string_right(tft_width - 1, 16, GRAY, buf);
-
 		tft_swap_buffers();
-		task_sleep_ms(3);
+		task_sleep_ms(9);
 		tft_sync();
 
 		uint32_t this_sync = time_us_32();
@@ -584,11 +380,6 @@ int main()
 	}
 
 	adc_init();
-
-	adc_gpio_init(26);
-	adc_gpio_init(27);
-	adc_gpio_init(28);
-	adc_gpio_init(29);
 
 	for (int i = 0; i < 16; i++)
 		srand(adc_read() + random());
