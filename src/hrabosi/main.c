894f513b32001569e55a788d70676d103da28b07
Jan Hamal Dvořák <mordae@anilinux.org>
2024-04-15 21:03:03 +0200

    hrabosi: add second player

--- src/hrabosi/main.c
+++ src/hrabosi/main.c
@@ -53,9 +53,22 @@ embed_sprite(s_green_tank_225, 16, 16, BLACK, "green_tank_225.data");
 embed_sprite(s_green_tank_270, 16, 16, BLACK, "green_tank_270.data");
 embed_sprite(s_green_tank_315, 16, 16, BLACK, "green_tank_315.data");
 
+embed_sprite(s_blue_tank_0, 16, 16, BLACK, "blue_tank_0.data");
+embed_sprite(s_blue_tank_45, 16, 16, BLACK, "blue_tank_45.data");
+embed_sprite(s_blue_tank_90, 16, 16, BLACK, "blue_tank_90.data");
+embed_sprite(s_blue_tank_135, 16, 16, BLACK, "blue_tank_135.data");
+embed_sprite(s_blue_tank_180, 16, 16, BLACK, "blue_tank_180.data");
+embed_sprite(s_blue_tank_225, 16, 16, BLACK, "blue_tank_225.data");
+embed_sprite(s_blue_tank_270, 16, 16, BLACK, "blue_tank_270.data");
+embed_sprite(s_blue_tank_315, 16, 16, BLACK, "blue_tank_315.data");
+
 static sdk_sprite_t ss_green_tank[8] = { &s_green_tank_0,   &s_green_tank_45,  &s_green_tank_90,
 					 &s_green_tank_135, &s_green_tank_180, &s_green_tank_225,
 					 &s_green_tank_270, &s_green_tank_315 };
+
+static sdk_sprite_t ss_blue_tank[8] = { &s_blue_tank_0,	  &s_blue_tank_45,  &s_blue_tank_90,
+					&s_blue_tank_135, &s_blue_tank_180, &s_blue_tank_225,
+					&s_blue_tank_270, &s_blue_tank_315 };
 
 inline static int dirt_color(int wx, int wy);
 inline static bool has_dirt(int wx, int wy);
@@ -67,11 +80,12 @@ struct tank {
 	float wx, wy;
 	float speed;
 	uint8_t angle;
-	uint8_t color;
 };
 
 static struct tank tank1;
+static struct tank tank2;
 
+#define TANK_SIZE 16
 #define TANK_SPEED 66.0f
 
 int main()
@@ -109,68 +123,142 @@ void game_reset(void)
 
 	tank1.wx = (int)((rand() % (WORLD_WIDTH * 6 / 8)) + WORLD_WIDTH / 8);
 	tank1.wy = (int)((rand() % (WORLD_HEIGHT * 6 / 8)) + WORLD_HEIGHT / 8);
-	tank1.color = GREEN;
 	tank1.speed = TANK_SPEED;
 
+	tank2.wx = (int)((rand() % (WORLD_WIDTH * 6 / 8)) + WORLD_WIDTH / 8);
+	tank2.wy = (int)((rand() % (WORLD_HEIGHT * 6 / 8)) + WORLD_HEIGHT / 8);
+	tank2.speed = TANK_SPEED;
+
+	/* TODO: Avoid tanks spawning too close. */
+
 	poke_hole(tank1.wx / CELL_SCALE, tank1.wy / CELL_SCALE, 12);
+	poke_hole(tank2.wx / CELL_SCALE, tank2.wy / CELL_SCALE, 12);
 }
 
 void game_input(unsigned dt_usec)
 {
 	float dt = dt_usec / 1000000.0;
-	float joy_x = dt * (sdk_inputs.joy_x + 64) / 2048.0;
-	float joy_y = dt * (sdk_inputs.joy_y + 64) / 2048.0;
 
-	float tank1_wx = tank1.wx;
-	float tank1_wy = tank1.wy;
+	{
+		sdk_inputs.joy_x = clamp((sdk_inputs.joy_x + 64) * 5 / 4, -2047, 2047);
+		sdk_inputs.joy_y = clamp((sdk_inputs.joy_y + 64) * 5 / 4, -2047, 2047);
 
-	tank1.speed = clamp(tank1.speed + TANK_SPEED * dt, 0, TANK_SPEED);
+		float joy_x = dt * sdk_inputs.joy_x / 2047.0f;
+		float joy_y = dt * sdk_inputs.joy_y / 2047.0f;
 
-	sdk_sprite_t t1s = ss_green_tank[tank1.angle];
-	int cells = poke_sprite(tank1.wx - t1s->w / 2.0f, tank1.wy - t1s->w / 2.0f, t1s);
-	tank1.speed /= (cells + 1);
+		float wx = tank1.wx;
+		float wy = tank1.wy;
 
-	if (!cells)
-		tank1.speed = clamp(tank1.speed * 1.2f, 0, TANK_SPEED);
+		tank1.speed = clamp(tank1.speed + TANK_SPEED * dt, 0, TANK_SPEED);
 
-	if (abs(sdk_inputs.joy_x) > 200)
-		tank1.wx = clamp(tank1.wx + joy_x * tank1.speed, 5, WORLD_RIGHT - 5);
+		sdk_sprite_t s = ss_green_tank[tank1.angle];
+		int cells = poke_sprite(tank1.wx - s->w / 2.0f, tank1.wy - s->w / 2.0f, s);
+		tank1.speed /= (cells + 1);
 
-	if (abs(sdk_inputs.joy_y) > 200)
-		tank1.wy = clamp(tank1.wy + joy_y * tank1.speed, 5, WORLD_BOTTOM - 5);
+		if (!cells)
+			tank1.speed = clamp(tank1.speed * 1.2f, 0, TANK_SPEED);
 
-	int tank1_angle = get_angle(tank1.wx - tank1_wx, tank1.wy - tank1_wy);
-	if (tank1_angle >= 0)
-		tank1.angle = tank1_angle;
+		if (abs(sdk_inputs.joy_x) > 200)
+			tank1.wx = clamp(tank1.wx + joy_x * tank1.speed, TANK_SIZE / 2.0f,
+					 WORLD_RIGHT - TANK_SIZE / 2.0f);
+
+		if (abs(sdk_inputs.joy_y) > 200)
+			tank1.wy = clamp(tank1.wy + joy_y * tank1.speed, TANK_SIZE / 2.0f,
+					 WORLD_BOTTOM - TANK_SIZE / 2.0f);
+
+		int angle = get_angle(tank1.wx - wx, tank1.wy - wy);
+		if (angle >= 0)
+			tank1.angle = angle;
+	}
+
+	{
+		float joy_x = dt * (sdk_inputs.b - sdk_inputs.x);
+		float joy_y = dt * (sdk_inputs.a - sdk_inputs.y);
+
+		float wx = tank2.wx;
+		float wy = tank2.wy;
+
+		tank2.speed = clamp(tank2.speed + TANK_SPEED * dt, 0, TANK_SPEED);
+
+		sdk_sprite_t s = ss_blue_tank[tank2.angle];
+		int cells = poke_sprite(tank2.wx - s->w / 2.0f, tank2.wy - s->w / 2.0f, s);
+		tank2.speed /= (cells + 1);
+
+		if (!cells)
+			tank2.speed = clamp(tank2.speed * 1.2f, 0, TANK_SPEED);
+
+		tank2.wx = clamp(tank2.wx + joy_x * tank2.speed, TANK_SIZE / 2.0f,
+				 WORLD_RIGHT - TANK_SIZE / 2.0f);
+		tank2.wy = clamp(tank2.wy + joy_y * tank2.speed, TANK_SIZE / 2.0f,
+				 WORLD_BOTTOM - TANK_SIZE / 2.0f);
+
+		int angle = get_angle(tank2.wx - wx, tank2.wy - wy);
+		if (angle >= 0)
+			tank2.angle = angle;
+	}
 }
 
 void game_paint(unsigned __unused dt_usec)
 {
-	int viewport_left = clamp(tank1.wx - TFT_WIDTH / 2.0f, 0, WORLD_RIGHT - TFT_WIDTH);
-	int viewport_top = clamp(tank1.wy - TFT_HEIGHT / 2.0f, 0, WORLD_BOTTOM - TFT_HEIGHT);
-	//int viewport_right = viewport_left + TFT_WIDTH - 1;
-	//int viewport_bottom = viewport_top + TFT_HEIGHT - 1;
+	const int pane_height = TFT_HEIGHT;
+	const int pane_width = TFT_WIDTH / 2 - 1;
+	const int pane_gap = 2;
 
-	for (int y = 0; y < TFT_HEIGHT; y++) {
-		/*
-		 * Painting the background takes a lot of time.
-		 * Make sure the input task can run unimpeded.
-		 */
-		sdk_yield_every_us(2000);
+	sdk_sprite_t green = ss_green_tank[tank1.angle];
+	sdk_sprite_t blue = ss_blue_tank[tank2.angle];
 
-		for (int x = 0; x < TFT_WIDTH; x++) {
-			int wx = viewport_left + x;
-			int wy = viewport_top + y;
+	int t1x = tank1.wx;
+	int t1y = tank1.wy;
 
-			tft_draw_pixel(x, y, has_dirt(wx, wy) ? dirt_color(wx, wy) : BLACK);
+	int t2x = tank2.wx;
+	int t2y = tank2.wy;
+
+	tft_draw_rect(pane_width, 0, pane_width + pane_gap, pane_height, DGRAY);
+
+	{
+		int wx = clamp(t1x - pane_width * 1 / 2, 0, WORLD_RIGHT - pane_width);
+		int wy = clamp(t1y - pane_height * 2 / 5, 0, WORLD_BOTTOM - pane_height);
+
+		tft_set_clip(0, 0, pane_width, pane_height);
+		tft_set_origin(wx, wy);
+
+		for (int y = wy; y < wy + pane_height; y++) {
+			sdk_yield_every_us(2000);
+
+			for (int x = wx; x < wx + pane_width; x++) {
+				bool dirt = has_dirt(x, y);
+				tft_draw_pixel(x, y, dirt ? dirt_color(x, y) : BLACK);
+			}
 		}
+
+		sdk_draw_sprite(t2x - blue->w / 2, t2y - blue->h / 2, blue);
+		sdk_draw_sprite(t1x - green->w / 2, t1y - green->h / 2, green);
+
+		tft_set_origin(0, 0);
+		tft_clear_clip();
 	}
 
 	{
-		sdk_sprite_t sprite = ss_green_tank[tank1.angle];
-		int vx = (int)tank1.wx - viewport_left - sprite->w / 2;
-		int vy = (int)tank1.wy - viewport_top - sprite->w / 2;
-		sdk_draw_sprite(vx, vy, ss_green_tank[tank1.angle]);
+		int wx = clamp(t2x - pane_width * 1 / 2, 0, WORLD_RIGHT - pane_width);
+		int wy = clamp(t2y - pane_height * 2 / 5, 0, WORLD_BOTTOM - pane_height);
+
+		tft_set_clip(pane_width + pane_gap, 0, TFT_WIDTH, pane_height);
+		tft_set_origin(wx - pane_gap - pane_width, wy);
+
+		for (int y = wy; y < wy + pane_height; y++) {
+			sdk_yield_every_us(2000);
+
+			for (int x = wx; x < wx + pane_width; x++) {
+				bool dirt = has_dirt(x, y);
+				tft_draw_pixel(x, y, dirt ? dirt_color(x, y) : BLACK);
+			}
+		}
+
+		sdk_draw_sprite(t1x - green->w / 2, t1y - green->h / 2, green);
+		sdk_draw_sprite(t2x - blue->w / 2, t2y - blue->h / 2, blue);
+
+		tft_set_origin(0, 0);
+		tft_clear_clip();
 	}
 }
 
@@ -192,6 +280,12 @@ inline static bool has_dirt(int wx, int wy)
 {
 	int gx = wx / CELL_SCALE;
 	int gy = wy / CELL_SCALE;
+
+	if (gx < 0 || gy < 0)
+		return false;
+
+	if (gx >= GRID_WIDTH || gy >= GRID_HEIGHT)
+		return false;
 
 	return (grid[gy][gx / 32] >> (gx & 31)) & 1;
 }
