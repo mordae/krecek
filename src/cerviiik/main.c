323d4fb9041d2b308b1d93ba431baf4ef5d4e17c
KOMHONZA323 <jan.honzik.jakoubek@gmail.com>
2024-11-02 15:13:33 +0100

    cerviiik: add music, winning screen

--- src/cerviiik/main.c
+++ src/cerviiik/main.c
@@ -15,12 +15,21 @@
 
 #define ANGLE_DELTA (5.0f / 180.0f * M_PI)
 
+enum screen {
+	GAME = 0,
+	SCORE,
+};
+
+static enum screen current_screen = GAME;
+static uint32_t start_time;
+
 struct worm {
 	float x, y;
 	float angle;
 	float speed;
 	uint8_t color;
 	bool alive;
+	uint32_t tod;
 };
 
 static int8_t grid[TFT_HEIGHT][TFT_WIDTH];
@@ -63,8 +72,36 @@ static struct worm worms_init[NUM_WORMS] = {
 
 };
 
+static uint16_t tones[256];
+static const char tune[] =
+	" ggahC g C g C g CChag D g D g D ggahC g C g C g CCDEF C F C F C FFEDC g C g C g CChag D g D g D";
+
+void game_start(void)
+{
+	sdk_set_output_gain_db(0);
+
+	tones['c'] = 131;
+	tones['d'] = 147;
+	tones['e'] = 165;
+	tones['f'] = 175;
+	tones['g'] = 196;
+	tones['a'] = 220;
+	tones['h'] = 247;
+
+	tones['C'] = 261;
+	tones['D'] = 293;
+	tones['E'] = 329;
+	tones['F'] = 349;
+	tones['G'] = 392;
+	tones['A'] = 440; // <--
+	tones['H'] = 494;
+}
+
 void game_reset(void)
 {
+	current_screen = GAME;
+	start_time = time_us_32();
+
 	for (int x = 0; x < TFT_WIDTH; x++) {
 		for (int y = 0; y < TFT_HEIGHT; y++) {
 			grid[y][x] = -1;
@@ -72,6 +109,37 @@ void game_reset(void)
 	}
 
 	memcpy(worms, worms_init, sizeof worms);
+}
+
+void game_audio(int nsamples)
+{
+	static int ellapsed = 0;
+	static int tone_pos = 0;
+
+	for (int s = 0; s < nsamples; s++) {
+		if (ellapsed > SDK_AUDIO_RATE / 4) {
+			tone_pos++;
+			ellapsed = 0;
+		}
+
+		if (!tune[tone_pos]) {
+			tone_pos = 0;
+		}
+
+		int freq = tones[(unsigned)tune[tone_pos]];
+
+		if (freq) {
+			int period = SDK_AUDIO_RATE / freq;
+			int half_period = 2 * ellapsed / period;
+			int modulo = half_period & 1;
+
+			sdk_write_sample(4000 * (modulo ? 1 : -1));
+		} else {
+			sdk_write_sample(0);
+		}
+
+		ellapsed++;
+	}
 }
 
 float angle_diff(float a, float b)
@@ -86,10 +154,8 @@ float angle_diff(float a, float b)
 	return diff;
 }
 
-void game_paint(unsigned __unused dt_usec)
+static void paint_game()
 {
-	tft_fill(0);
-
 	tft_draw_rect(0, 0, 0, TFT_HEIGHT - 1, 7);
 	tft_draw_rect(0, 0, TFT_WIDTH - 1, 0, 7);
 	tft_draw_rect(TFT_WIDTH - 1, 0, TFT_WIDTH - 1, TFT_HEIGHT - 1, 7);
@@ -190,10 +256,60 @@ void game_paint(unsigned __unused dt_usec)
 
 		if (worms[i].y < 0 || worms[i].y > TFT_HEIGHT)
 			worms[i].alive = false;
+
+		if (worms[i].alive)
+			worms[i].tod = time_us_32() - start_time;
 	}
 
-	if (worms_alive <= 1)
+	if (worms_alive <= 1) {
+		current_screen = SCORE;
+	}
+}
+
+static int pick_next_loser(uint32_t tod_better_than)
+{
+	uint32_t worst_tod = UINT32_MAX;
+	int worst = 0;
+
+	for (int i = 0; i < NUM_WORMS; i++) {
+		if (worms[i].tod > tod_better_than && worms[i].tod < worst_tod) {
+			worst_tod = worms[i].tod;
+			worst = i;
+		}
+	}
+
+	return worst;
+}
+
+static void paint_score()
+{
+	if (sdk_inputs.start) {
 		game_reset();
+	}
+
+	int loser4 = pick_next_loser(0);
+	int loser3 = pick_next_loser(worms[loser4].tod);
+	int loser2 = pick_next_loser(worms[loser3].tod);
+	int loser1 = pick_next_loser(worms[loser2].tod);
+
+	tft_draw_rect(0, 0, TFT_RIGHT, TFT_BOTTOM, worms[loser4].color);
+	tft_draw_rect(TFT_WIDTH / 8, TFT_HEIGHT / 8, TFT_RIGHT - TFT_WIDTH / 8,
+		      TFT_BOTTOM - TFT_HEIGHT / 8, worms[loser3].color);
+	tft_draw_rect(2 * TFT_WIDTH / 8, 2 * TFT_HEIGHT / 8, TFT_RIGHT - 2 * TFT_WIDTH / 8,
+		      TFT_BOTTOM - 2 * TFT_HEIGHT / 8, worms[loser2].color);
+	tft_draw_rect(3 * TFT_WIDTH / 8, 3 * TFT_HEIGHT / 8, TFT_RIGHT - 3 * TFT_WIDTH / 8,
+		      TFT_BOTTOM - 3 * TFT_HEIGHT / 8, worms[loser1].color);
+}
+
+void game_paint(unsigned __unused dt_usec)
+{
+	tft_fill(0);
+
+	if (current_screen == GAME) {
+		paint_game();
+	} else if (current_screen == SCORE) {
+		paint_score();
+	}
 }
 
 int main()
