48ca287a2dda53f1e373ea913d8e386d37df3454
Jan Hamal Dvořák <mordae@anilinux.org>
2024-11-02 12:33:18 +0100

    Add tools/clangd_host.py

---
+++ tools/clangd_host.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+from glob import glob
+
+import click
+import yaml
+
+
+@click.command()
+def clangd():
+    """Generate .clangd file for local development."""
+
+    src = os.path.realpath(os.path.join(os.getcwd(), "src"))
+    host = os.path.realpath(os.path.join(os.getcwd(), "host"))
+
+    includes = [
+        *glob(f"{src}/**/include", recursive=True),
+        f"{host}/sdk/include",
+        f"{src}/sdk/include",
+        f"{src}/vendor/pico-tft/include",
+    ]
+
+    flags = [
+        "-Wall",
+        "-Wextra",
+        "-xc",
+        "-DCFG_TUSB_MCU=OPT_MCU_RP2040",
+        "-I/usr/include/SDL2",
+        "-I/usr/include/freetype2",
+        "-DTFT_SCALE=2",
+        "-DTFT_DRIVE=TFT_DRIVER_ILI9341",
+        "-DTFT_VSYNC=1",
+        "-DTFT_SCALE=2",
+        "-DTFT_SWAP_XY=1",
+    ]
+
+    yaml.safe_dump(
+        {
+            "CompileFlags": {
+                "Compiler": "gcc",
+                "Add": [*flags, *[f"-I{inc}" for inc in includes]],
+            }
+        },
+        sys.stdout,
+    )
+
+
+if __name__ == "__main__":
+    clangd()
