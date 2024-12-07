b4149a3560f115d9f5b47da6b5b9293978fce9db
Rafailek <rafaelmohl.rez@gmail.com>
2024-12-07 16:43:54 +0100

    krectris: finish the base mechanics

--- src/krectris/main.c
+++ src/krectris/main.c
@@ -28,10 +28,12 @@
 #define BOARD_HEIGHT 20
 
 #define BOARD_OFFSET_X 60
-#define BOARD_OFFSET_Y -68
+#define BOARD_OFFSET_Y -60
 
 #define NEXT_LIST_POS_X 11
 #define NEXT_LIST_POS_Y 20
+#define HOLD_PIECE_POS_X -5
+#define HOLD_PIECE_POS_Y 20
 
 static uint8_t board[400] = {
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -81,6 +83,13 @@ static int active_piece_x = 0;
 static int active_piece_y = BOARD_HEIGHT;
 static int active_piece_orientation = 0;
 
+static int ghost_piece_shape = 0;
+static int ghost_piece_x = 0;
+static int ghost_piece_y = 0;
+static int ghost_piece_orientation = 0;
+
+static int hold_piece = -1; // -1 when hold piece empty
+
 static int movement_lr_das = 150000;
 static int movement_lr_arr =  33333;
 static int time_moved_lr = 0;
@@ -89,154 +98,141 @@ static int gravity_speed = 1000000; // how much time passes before the active pi
 static int gravity_strength = 1; // how many spaces does the active piece fall
 static int time_after_falling = 0;
 
+static int lock_time = 500000; // how much time you have on the ground before the piece locks
+static int lock_movement = 15; // how many times you can move on the ground before the piece locks
+static int lock_time_elapsed = 0;
+static int lock_movements_used = 0;
+static int lowest_row_reached = 0;
+
 static int piece_colors[7] = {
 	RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE
 };
+static int piece_spawn_x[7] = {3,  3,  3,  3,  3,  3,  3};
+static int piece_spawn_y[7] = {18, 18, 17, 18, 18, 18, 18};
 
 static int active_bag[7] = {0, 1, 2, 3, 4, 5, 6};
 static int future_bag[7] = {0, 1, 2, 3, 4, 5, 6};
 static int next_piece_in_bag = 6;
 
-static uint8_t piece_rotation[448] = { //todo: store in a more space efficient format
-	1, 1, 0, 0, // z
-	0, 1, 1, 0,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-	
-	0, 0, 1, 0,
-	0, 1, 1, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
+static uint8_t piece_rotation[112] = { //todo: store in a more space efficient format
+	0b1100, // z
+	0b0110,
+	0b0000,
+	0b0000,
+	0b0010,
+	0b0110,
+	0b0100,
+	0b0000,
+	0b0000,
+	0b1100,
+	0b0110,
+	0b0000,
+	0b0100,
+	0b1100,
+	0b1000,
+	0b0000,
 
-	0, 0, 0, 0,
-	1, 1, 0, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
+	0b0010, // l
+	0b1110,
+	0b0000,
+	0b0000,
+	0b0100,
+	0b0100,
+	0b0110,
+	0b0000,
+	0b0000,
+	0b1110,
+	0b1000,
+	0b0000,
+	0b1100,
+	0b0100,
+	0b0100,
+	0b0000,
 
-	0, 1, 0, 0,
-	1, 1, 0, 0,
-	1, 0, 0, 0,
-	0, 0, 0, 0,
+	0b0000, // o
+	0b0110,
+	0b0110,
+	0b0000,
+	0b0000,
+	0b0110,
+	0b0110,
+	0b0000,
+	0b0000,
+	0b0110,
+	0b0110,
+	0b0000,
+	0b0000,
+	0b0110,
+	0b0110,
+	0b0000,
 
-	0, 0, 1, 0, // l
-	1, 1, 1, 0,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
+	0b0110, // s
+	0b1100,
+	0b0000,
+	0b0000,
+	0b0100,
+	0b0110,
+	0b0010,
+	0b0000,
+	0b0000,
+	0b0110,
+	0b1100,
+	0b0000,
+	0b1000,
+	0b1100,
+	0b0100,
+	0b0000,
 
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
+	0b0000, // i
+	0b1111,
+	0b0000,
+	0b0000,
+	0b0010,
+	0b0010,
+	0b0010,
+	0b0010,
+	0b0000,
+	0b0000,
+	0b1111,
+	0b0000,
+	0b0100,
+	0b0100,
+	0b0100,
+	0b0100,
 
-	0, 0, 0, 0,
-	1, 1, 1, 0,
-	1, 0, 0, 0,
-	0, 0, 0, 0,
+	0b1000, // j
+	0b1110,
+	0b0000,
+	0b0000,
+	0b0110,
+	0b0100,
+	0b0100,
+	0b0000,
+	0b0000,
+	0b1110,
+	0b0010,
+	0b0000,
+	0b0100,
+	0b0100,
+	0b1100,
+	0b0000,
 
-	1, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0, // o
-	0, 1, 1, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	0, 1, 1, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	0, 1, 1, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	0, 1, 1, 0,
-	0, 1, 1, 0,
-	0, 0, 0, 0,
-
-	0, 1, 1, 0, // s
-	1, 1, 0, 0,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0,
-	0, 1, 1, 0,
-	0, 0, 1, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	0, 1, 1, 0,
-	1, 1, 0, 0,
-	0, 0, 0, 0,
-
-	1, 0, 0, 0,
-	1, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0, // i
-	1, 1, 1, 1,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-
-	0, 0, 1, 0,
-	0, 0, 1, 0,
-	0, 0, 1, 0,
-	0, 0, 1, 0,
-
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-	1, 1, 1, 1,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-
-	1, 0, 0, 0, // j
-	1, 1, 1, 0,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-
-	0, 1, 1, 0,
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	1, 1, 1, 0,
-	0, 0, 1, 0,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0,
-	0, 1, 0, 0,
-	1, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0, // t
-	1, 1, 1, 0,
-	0, 0, 0, 0,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0,
-	0, 1, 1, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 0, 0, 0,
-	1, 1, 1, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
-
-	0, 1, 0, 0,
-	1, 1, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 0, 0,
+	0b0100, // t
+	0b1110,
+	0b0000,
+	0b0000,
+	0b0100,
+	0b0110,
+	0b0100,
+	0b0000,
+	0b0000,
+	0b1110,
+	0b0100,
+	0b0000,
+	0b0100,
+	0b1100,
+	0b0100,
+	0b0000
 };
 
 static int8_t kick_table_x_clockwise[20] = { // based on orientation BEFORE rotation
@@ -295,7 +291,7 @@ static int8_t kick_table_I_y_counterclockwise[20] = {
 	0, 0, 0, 2, -1,
 };
 
-static void draw_mino(int x, int y, int color) {
+static void draw_mino(int x, int y, int color, bool ghost) {
 	if (color == 0) {
 		if (y >= BOARD_HEIGHT) {
 			tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
@@ -307,13 +303,24 @@ static void draw_mino(int x, int y, int color) {
 				      (SPACE_SIZE * y + SPACE_SIZE - 2) + BOARD_OFFSET_Y, BLACK);
 		}
 	} else {
-	tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
-		      (SPACE_SIZE * x + SPACE_SIZE - 1) + BOARD_OFFSET_X,
-		      (SPACE_SIZE * y + SPACE_SIZE - 1) + BOARD_OFFSET_Y, BLACK);
+		if (!ghost) {
+			tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
+				      (SPACE_SIZE * x + SPACE_SIZE - 1) + BOARD_OFFSET_X,
+				      (SPACE_SIZE * y + SPACE_SIZE - 1) + BOARD_OFFSET_Y, BLACK);
 	
-	tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
-		      (SPACE_SIZE * x + SPACE_SIZE - 2) + BOARD_OFFSET_X,
-		      (SPACE_SIZE * y + SPACE_SIZE - 2) + BOARD_OFFSET_Y, color);
+			tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
+				      (SPACE_SIZE * x + SPACE_SIZE - 2) + BOARD_OFFSET_X,
+				      (SPACE_SIZE * y + SPACE_SIZE - 2) + BOARD_OFFSET_Y, color);
+		} else {
+			
+			tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
+				      (SPACE_SIZE * x + SPACE_SIZE - 1) + BOARD_OFFSET_X,
+				      (SPACE_SIZE * y + SPACE_SIZE - 1) + BOARD_OFFSET_Y, color);
+	
+			tft_draw_rect(SPACE_SIZE * x + BOARD_OFFSET_X, SPACE_SIZE * y + BOARD_OFFSET_Y,
+				      (SPACE_SIZE * x + SPACE_SIZE - 2) + BOARD_OFFSET_X,
+				      (SPACE_SIZE * y + SPACE_SIZE - 2) + BOARD_OFFSET_Y, BLACK);
+		}
 	}
 }
 
@@ -322,7 +329,7 @@ static int lookup_rotation_table(int x, int y, int shape, int orientation) {
 	assert(orientation >= 0 && orientation <= 3);
 	assert(x >= 0 && x <= 3);
 	assert(y >= 0 && y <= 3);
-	if (piece_rotation[shape * 64 + orientation * 16 + y * 4 + x] == 1)
+	if ((piece_rotation[shape * 16 + orientation * 4 + y] & (8 >> x)) != 0)
 		return 1;
 	else
         	return 0;
@@ -409,12 +416,15 @@ static void rotate_active_piece_I_counterclockwise() {
 	}
 }
 
-static void lock_active_piece() {
-	for (int y = 0; y <= 3; y++) {
-		for (int x = 0; x <= 3; x++) {
-			if (lookup_rotation_table(x, y, active_piece_shape, active_piece_orientation) == 1) {
-				board[(active_piece_y + y) * BOARD_WIDTH + active_piece_x + x] = piece_colors[active_piece_shape];
-			}
+static void detirmine_ghost_position() {
+	ghost_piece_orientation = active_piece_orientation;
+	ghost_piece_shape = active_piece_shape;
+	ghost_piece_x = active_piece_x;
+	ghost_piece_y = active_piece_y;
+
+	for (int i = 0; i <= 20; i++) {
+		if (verify_piece_placement(ghost_piece_x, ghost_piece_y + 1, ghost_piece_shape, ghost_piece_orientation)) {
+			ghost_piece_y++;
 		}
 	}
 }
@@ -442,6 +452,62 @@ static void generate_future_bag() {
 	}
 }
 
+static bool is_on_ground() {
+	if (!verify_piece_placement(active_piece_x, active_piece_y + 1, active_piece_shape, active_piece_orientation)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static void lock_active_piece() {
+	detirmine_ghost_position();
+	active_piece_y = ghost_piece_y;
+	for (int y = 0; y <= 3; y++) {
+		for (int x = 0; x <= 3; x++) {
+			if (lookup_rotation_table(x, y, active_piece_shape, active_piece_orientation) == 1) {
+				board[(active_piece_y + y) * BOARD_WIDTH + active_piece_x + x] = piece_colors[active_piece_shape];
+			}
+		}
+	}
+	active_piece_orientation = 0;
+	active_piece_shape = active_bag[next_piece_in_bag];
+	active_piece_x = piece_spawn_x[active_piece_shape];
+	active_piece_y = piece_spawn_y[active_piece_shape];
+	lock_movements_used = 0;
+
+	next_piece_in_bag--;
+	
+	if (next_piece_in_bag < 0) {
+		next_piece_in_bag = 6;
+		for (int i = 0; i <= 6; i++) {
+			active_bag[i] = future_bag[i];
+		}
+		generate_future_bag();
+		next_piece_in_bag = 6;
+	}
+
+	for (int i = 0; i <= 3; i++) {
+		int row_to_remove = -1;
+		for (int row = BOARD_HEIGHT * 2 - 1; row >= 0; row--) {
+			if (is_row_full(row)) {
+				row_to_remove = row;
+				break;
+			}
+		}
+		if (row_to_remove >= 0) {
+			for (int y = row_to_remove; y >= 1; y--) {
+				for (int x = 0; x <= BOARD_WIDTH - 1; x++) {
+					board[y * BOARD_WIDTH + x] = board[(y - 1) * BOARD_WIDTH + x];
+				}
+			}
+			for (int x = 0; x <= BOARD_WIDTH - 1; x++)
+				board[x] = 0;
+		} else {
+			break;
+		}
+	}
+}
 
 void game_reset(void)
 {
@@ -452,6 +518,9 @@ void game_reset(void)
 	generate_future_bag();
 	active_piece_shape = active_bag[next_piece_in_bag];
 	next_piece_in_bag--;
+
+	active_piece_x = piece_spawn_x[active_piece_shape];
+	active_piece_y = piece_spawn_y[active_piece_shape];
 }
 
 void game_start(void)
@@ -465,6 +534,19 @@ void game_audio(int __unused nsamples)
 
 void game_input(unsigned dt_usec)
 {
+	if (is_on_ground()) {
+		lock_time_elapsed += dt_usec;
+		if (lock_time_elapsed > lock_time) {
+			lock_active_piece();
+		}
+	} else {
+		lock_time_elapsed = 0;
+		if (active_piece_y > lowest_row_reached) {
+			lowest_row_reached = active_piece_y;
+			lock_movements_used = 0;
+		}
+	}
+	
 	if (sdk_inputs.joy_x > 1024 || sdk_inputs.joy_x < -1024) {
 		if (time_moved_lr == 0) {
 			if (sdk_inputs.joy_x > 0)
@@ -472,6 +554,13 @@ void game_input(unsigned dt_usec)
 			else
 				move_active_piece(-1, 0);
 			time_moved_lr++;
+			if (is_on_ground()) {
+				lock_movements_used++;
+				lock_time_elapsed = 0;
+			}
+			if (lock_movements_used > lock_movement) {
+				lock_active_piece();
+			}
 		} else {
 			if (time_moved_lr > movement_lr_das && time_moved_lr - movement_lr_das > movement_lr_arr) {
 				if (sdk_inputs.joy_x > 0)
@@ -479,7 +568,13 @@ void game_input(unsigned dt_usec)
 				else
 					move_active_piece(-1, 0);
 				time_moved_lr -= movement_lr_arr + dt_usec;
-				
+				if (is_on_ground()) {
+					lock_movements_used++;
+					lock_time_elapsed = 0;
+				}
+				if (lock_movements_used > lock_movement) {
+					lock_active_piece();
+				}
 			}
 		}
 		time_moved_lr += dt_usec;
@@ -501,60 +596,63 @@ void game_input(unsigned dt_usec)
 			rotate_active_piece_clockwise();
 		else
 			rotate_active_piece_I_clockwise();
+
+		if (is_on_ground()) {
+			lock_movements_used++;
+			lock_time_elapsed = 0;
+		}
+		if (lock_movements_used > lock_movement) {
+			lock_active_piece();
+		}
 	}
 	if (sdk_inputs_delta.a > 0) {
 		if (active_piece_shape != 4)
 			rotate_active_piece_counterclockwise();
 		else
 			rotate_active_piece_I_counterclockwise();
+
+		if (is_on_ground()) {
+			lock_movements_used++;
+			lock_time_elapsed = 0;
+		}
+		if (lock_movements_used > lock_movement) {
+			lock_active_piece();
+		}
 	}
 
-	if (sdk_inputs_delta.y > 0)
-		active_piece_shape++;
-	if (active_piece_shape > 6)
-		active_piece_shape = 0;
+	if (sdk_inputs_delta.y > 0) {
+		if (hold_piece == -1) { // hold used for the first time
+			hold_piece = active_piece_shape;
+			
+			active_piece_orientation = 0;
+			active_piece_shape = active_bag[next_piece_in_bag];
+			active_piece_x = piece_spawn_x[active_piece_shape];
+			active_piece_y = piece_spawn_y[active_piece_shape];
+
+			next_piece_in_bag--;
+		
+			if (next_piece_in_bag < 0) {
+				next_piece_in_bag = 6;
+				for (int i = 0; i <= 6; i++) {
+					active_bag[i] = future_bag[i];
+				}
+				generate_future_bag();
+				next_piece_in_bag = 6;
+			}
+		} else {
+			int new_piece;
+			new_piece = hold_piece;
+			hold_piece = active_piece_shape;
+			
+			active_piece_orientation = 0;
+			active_piece_shape = new_piece;
+			active_piece_x = piece_spawn_x[active_piece_shape];
+			active_piece_y = piece_spawn_y[active_piece_shape];
+		}
+	}
 
 	if (sdk_inputs_delta.x > 0) {
-		for (int i = 0; i <= BOARD_HEIGHT * 2 - 1; i++) {
-			move_active_piece(0, 1);
-		}
 		lock_active_piece();
-		active_piece_x = 0;
-		active_piece_y = BOARD_HEIGHT;
-		active_piece_orientation = 0;
-		active_piece_shape = active_bag[next_piece_in_bag];
-
-		next_piece_in_bag--;
-		
-		if (next_piece_in_bag < 0) {
-			next_piece_in_bag = 6;
-			for (int i = 0; i <= 6; i++) {
-				active_bag[i] = future_bag[i];
-			}
-			generate_future_bag();
-			next_piece_in_bag = 6;
-		}
-
-		for (int i = 0; i <= 3; i++) {
-			int row_to_remove = -1;
-			for (int row = BOARD_HEIGHT * 2 - 1; row >= 0; row--) {
-				if (is_row_full(row)) {
-					row_to_remove = row;
-					break;
-				}
-			}
-			if (row_to_remove >= 0) {
-				for (int y = row_to_remove; y >= 1; y--) {
-					for (int x = 0; x <= BOARD_WIDTH - 1; x++) {
-						board[y * BOARD_WIDTH + x] = board[(y - 1) * BOARD_WIDTH + x];
-					}
-				}
-				for (int x = 0; x <= BOARD_WIDTH - 1; x++)
-					board[x] = 0;
-			} else {
-				break;
-			}
-		}
 	}
 }
 
@@ -564,7 +662,7 @@ void game_paint(unsigned __unused dt_usec)
 
 	for (int y = 0; y <= BOARD_HEIGHT * 2 - 1; y++) {
 		for (int x = 0; x <= BOARD_WIDTH - 1; x++) {
-			draw_mino(x, y, board[y * BOARD_WIDTH + x]);
+			draw_mino(x, y, board[y * BOARD_WIDTH + x], false);
 		}
 	}
 
@@ -573,10 +671,18 @@ void game_paint(unsigned __unused dt_usec)
 	tft_draw_rect(BOARD_OFFSET_X - 1, BOARD_OFFSET_Y + (BOARD_HEIGHT * SPACE_SIZE) + (BOARD_HEIGHT * SPACE_SIZE), BOARD_OFFSET_X + (BOARD_WIDTH * SPACE_SIZE), BOARD_OFFSET_Y + (BOARD_HEIGHT * SPACE_SIZE) + (BOARD_HEIGHT * SPACE_SIZE), 223);
 	tft_draw_rect(BOARD_OFFSET_X + (BOARD_WIDTH * SPACE_SIZE), BOARD_OFFSET_Y - 1 + (BOARD_HEIGHT * SPACE_SIZE), BOARD_OFFSET_X + (BOARD_WIDTH * SPACE_SIZE), BOARD_OFFSET_Y + (BOARD_HEIGHT * SPACE_SIZE) + (BOARD_HEIGHT * SPACE_SIZE), 223);
 
+	detirmine_ghost_position();
+	for (int y = 0; y <= 3; y++) {
+		for (int x = 0; x <= 3; x++) {
+			if (lookup_rotation_table(x, y, ghost_piece_shape, ghost_piece_orientation) == 1)
+				draw_mino(ghost_piece_x + x, ghost_piece_y + y, piece_colors[active_piece_shape], true);
+		}
+	}
+
 	for (int y = 0; y <= 3; y++) {
 		for (int x = 0; x <= 3; x++) {
 			if (lookup_rotation_table(x, y, active_piece_shape, active_piece_orientation) == 1)
-				draw_mino(active_piece_x + x, active_piece_y + y, piece_colors[active_piece_shape]);
+				draw_mino(active_piece_x + x, active_piece_y + y, piece_colors[active_piece_shape], false);
 		}
 	}
 
@@ -584,10 +690,9 @@ void game_paint(unsigned __unused dt_usec)
 	for (int y = 0; y <= 3; y++) {
 		for (int x = 0; x <= 3; x++) {
 			if (lookup_rotation_table(x, y, active_bag[next_piece], 0) == 1)
-				draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y, piece_colors[active_bag[next_piece]]);
+				draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y, piece_colors[active_bag[next_piece]], false);
 		}
 	}
-
 	bool use_future_bag = false;
 	for (int i = 1; i <= 3; i++) {
 		next_piece--;
@@ -598,16 +703,25 @@ void game_paint(unsigned __unused dt_usec)
 		if (!use_future_bag) {
 			for (int y = 0; y <= 3; y++) {
 				for (int x = 0; x <= 3; x++) {
-					if (lookup_rotation_table(x, y, future_bag[next_piece], 0) == 1)
-						draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y + (i * 4), piece_colors[future_bag[next_piece]]);
+					if (lookup_rotation_table(x, y, active_bag[next_piece], 0) == 1)
+						draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y + (i * 4), piece_colors[active_bag[next_piece]], false);
 				}
 			}
 		} else {
 			for (int y = 0; y <= 3; y++) {
 				for (int x = 0; x <= 3; x++) {
-					if (lookup_rotation_table(x, y, active_bag[next_piece], 0) == 1)
-						draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y + (i * 4), piece_colors[active_bag[next_piece]]);
+					if (lookup_rotation_table(x, y, future_bag[next_piece], 0) == 1)
+						draw_mino(NEXT_LIST_POS_X + x, NEXT_LIST_POS_Y + y + (i * 4), piece_colors[future_bag[next_piece]], false);
 				}
+			}
+		}
+	}
+
+	if (hold_piece != -1) {
+		for (int y = 0; y <= 3; y++) {
+			for (int x = 0; x <= 3; x++) {
+				if (lookup_rotation_table(x, y, hold_piece, 0) == 1)
+					draw_mino(HOLD_PIECE_POS_X + x, HOLD_PIECE_POS_Y + y, piece_colors[hold_piece], false);
 			}
 		}
 	}
