50b7a5a0ed2c0a87dd5d0045dcc121e1e9a25c06
Jan Hamal Dvořák <mordae@anilinux.org>
2025-03-01 15:40:16 +0100

    Fix colors in demolice

--- src/demolice/main.c
+++ src/demolice/main.c
@@ -1,11 +1,9 @@
 #include "sdk/game.h"
 #include "sdk/input.h"
-#include "sdk/util.h"
 #include <math.h>
 #include <pico/stdlib.h>
 
 #include <sdk.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <tft.h>
 
@@ -18,12 +16,12 @@
 
 #define BALL_MOVEMENT_SUBSTEPS 4
 
-#define RED 239
-#define YELLOW 226
-#define GREEN 228
-#define BLUE 233
-#define GRAY 8
-#define WHITE 15
+#define RED rgb_to_rgb565(255, 0, 0)
+#define YELLOW rgb_to_rgb565(255, 255, 0)
+#define GREEN rgb_to_rgb565(0, 255, 0)
+#define BLUE rgb_to_rgb565(0, 0, 255)
+#define GRAY rgb_to_rgb565(127, 127, 127)
+#define WHITE rgb_to_rgb565(255, 255, 255)
 
 struct vector_2 {
 	float x;
@@ -44,18 +42,19 @@ struct ball {
 static struct ball ball;
 
 static int blocks[BLOCK_COUNT_Y][BLOCK_COUNT_X];
-static int block_colors[4] = {RED, YELLOW, GREEN, BLUE};
+static int block_colors[4] = { RED, YELLOW, GREEN, BLUE };
 
-static struct vector_2 block_position = {
-	.x = (float)(TFT_WIDTH - BLOCK_WIDTH * BLOCK_COUNT_X) / 2,
-	.y = (float)(TFT_WIDTH - BLOCK_WIDTH * BLOCK_COUNT_X) / 2
-};
+static struct vector_2 block_position = { .x = (float)(TFT_WIDTH - BLOCK_WIDTH * BLOCK_COUNT_X) / 2,
+					  .y = (float)(TFT_WIDTH - BLOCK_WIDTH * BLOCK_COUNT_X) /
+					       2 };
 
-float get_vector_magnitude(struct vector_2 v) {
+float get_vector_magnitude(struct vector_2 v)
+{
 	return sqrtf(v.x * v.x + v.y * v.y);
 }
 
-struct vector_2 normalise(struct vector_2 v) {
+struct vector_2 normalise(struct vector_2 v)
+{
 	float m = get_vector_magnitude(v);
 	if (!m) {
 		return v;
@@ -66,7 +65,8 @@ struct vector_2 normalise(struct vector_2 v) {
 	return ov;
 }
 
-bool is_position_full(struct vector_2 p) {
+bool is_position_full(struct vector_2 p)
+{
 	if (p.x < 0 || p.x >= TFT_WIDTH) {
 		return true;
 	}
@@ -76,7 +76,8 @@ bool is_position_full(struct vector_2 p) {
 	if (p.y >= TFT_HEIGHT) {
 		game_reset();
 	}
-	if (abs((int)(p.x - paddle.position)) < PADDLE_WIDTH / 2 && p.y > TFT_BOTTOM - PADDLE_HEIGHT) {
+	if (abs((int)(p.x - paddle.position)) < PADDLE_WIDTH / 2 &&
+	    p.y > TFT_BOTTOM - PADDLE_HEIGHT) {
 		ball.direction.y = -1 + abs((int)(p.x - paddle.position)) / PADDLE_WIDTH;
 		ball.direction.x = (p.x - paddle.position) / PADDLE_WIDTH;
 		return false;
@@ -89,19 +90,20 @@ bool is_position_full(struct vector_2 p) {
 	if (p.x > BLOCK_COUNT_X - 1 || p.y > BLOCK_COUNT_Y - 1) {
 		return false;
 	}
-	if (blocks[(int) p.y][(int) p.x]) {
-		blocks[(int) p.y][(int) p.x] = 0;
+	if (blocks[(int)p.y][(int)p.x]) {
+		blocks[(int)p.y][(int)p.x] = 0;
 		ball.speed += 1;
 		return true;
 	}
 	return false;
 }
 
-void handle_ball_collision() {
-	struct vector_2 pu = {ball.position.x, ball.position.y - 1};
-	struct vector_2 pd = {ball.position.x, ball.position.y + 1};
-	struct vector_2 pl = {ball.position.x - 1, ball.position.y};
-	struct vector_2 pr = {ball.position.x + 1, ball.position.y};
+void handle_ball_collision()
+{
+	struct vector_2 pu = { ball.position.x, ball.position.y - 1 };
+	struct vector_2 pd = { ball.position.x, ball.position.y + 1 };
+	struct vector_2 pl = { ball.position.x - 1, ball.position.y };
+	struct vector_2 pr = { ball.position.x + 1, ball.position.y };
 	if (is_position_full(pu) && ball.direction.y < 0) {
 		ball.direction.y *= -1;
 	}
@@ -121,11 +123,9 @@ void game_reset(void)
 	paddle.position = (float)TFT_WIDTH / 2;
 	paddle.speed = 100;
 
-	ball = (struct ball){
-		.position = (struct vector_2){5, 5},
-		.direction = (struct vector_2){0.2, 0.4},
-		.speed = 60 
-	};
+	ball = (struct ball){ .position = (struct vector_2){ 5, 5 },
+			      .direction = (struct vector_2){ 0.2, 0.4 },
+			      .speed = 60 };
 
 	for (int y = 0; y < BLOCK_COUNT_Y; y++) {
 		for (int x = 0; x < BLOCK_COUNT_X; x++) {
@@ -159,33 +159,26 @@ void game_input(unsigned dt_usec)
 	}
 
 	// printf("x: %.2f, y: %.2f\n", ball.position.x, ball.position.y);
-
 }
 
 void game_paint(unsigned __unused dt_usec)
 {
 	tft_fill(0);
 
-	tft_draw_rect(paddle.position - PADDLE_WIDTH / 2,
-		      TFT_BOTTOM - PADDLE_HEIGHT,
-		      paddle.position + PADDLE_WIDTH / 2,
-		      TFT_BOTTOM,
-		      WHITE);
+	tft_draw_rect(paddle.position - PADDLE_WIDTH / 2, TFT_BOTTOM - PADDLE_HEIGHT,
+		      paddle.position + PADDLE_WIDTH / 2, TFT_BOTTOM, WHITE);
 
 	for (int y = 0; y < BLOCK_COUNT_Y; y++) {
 		for (int x = 0; x < BLOCK_COUNT_X; x++) {
-			tft_draw_rect(block_position.x + x * BLOCK_WIDTH, block_position.y + y * BLOCK_HEIGHT,
-			              block_position.x + (x + 1) * BLOCK_WIDTH - 1,
-			              block_position.y + (y + 1) * BLOCK_HEIGHT - 1,
-			              blocks[y][x]);
+			tft_draw_rect(block_position.x + x * BLOCK_WIDTH,
+				      block_position.y + y * BLOCK_HEIGHT,
+				      block_position.x + (x + 1) * BLOCK_WIDTH - 1,
+				      block_position.y + (y + 1) * BLOCK_HEIGHT - 1, blocks[y][x]);
 		}
 	}
 
-	tft_draw_rect(ball.position.x - 1,
-		      ball.position.y - 1,
-		      ball.position.x + 1,
-		      ball.position.y + 1,
-		      WHITE);
+	tft_draw_rect(ball.position.x - 1, ball.position.y - 1, ball.position.x + 1,
+		      ball.position.y + 1, WHITE);
 }
 
 int main()
