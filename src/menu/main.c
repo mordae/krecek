bed24c11e32a8756430056795714bf394bccb526
Maxim Herman <mxmhrmn@gmail.com>
2025-03-12 19:03:57 +0100

    work on menu

--- src/menu/main.c
+++ src/menu/main.c
@@ -4,11 +4,13 @@
 #include <tft.h>
 
 #include <icons.png.h>
+#include <text.png.h>
 
 #define GRAY rgb_to_rgb565(63, 63, 63)
 
+// button
 struct b {
-	float s;
+	float s; //select
 };
 
 static struct b b;
@@ -30,9 +32,23 @@ void game_reset(void)
 	b.s = 0;
 }
 
+void game_input(unsigned dt_usec)
+{
+	if (sdk_inputs_delta.b > 0 || sdk_inputs.joy_x > 300) {
+		b.s += 1;
+	} else if (sdk_inputs_delta.x > 0 || sdk_inputs.joy_x < -300) {
+		b.s -= 1;
+	}
+}
+
 void game_paint(unsigned __unused dt_usec)
 {
-	sdk_draw_tile(0, 0, &ts_icons_png, 0);
+	tft_fill(0);
+
+	sdk_draw_tile(0 + b.s, 0, &ts_text_png, 0);
+	sdk_draw_tile(35, 15, &ts_icons_png, b.s);
+	sdk_draw_tile(-65, 20, &ts_icons_png, b.s - 1);
+	sdk_draw_tile(135, 20, &ts_icons_png, b.s + 1);
 }
 
 int main()
