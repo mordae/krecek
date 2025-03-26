c5bf1f1bbfc5a81b8148bc27628c819caa838484
darkest_fear <pizduchab@gmail.com>
2025-03-26 18:39:06 +0100

    experiment: add animated character

--- src/experiment/main.c
+++ src/experiment/main.c
@@ -1,4 +1,5 @@
 #include <pico/stdlib.h>
+#include <math.h>
 
 #include <sdk.h>
 #include <tft.h>
@@ -6,14 +7,29 @@
 #include "common.h"
 
 #include <tileset.png.h>
+#include <player.png.h>
 
 #define GRAY rgb_to_rgb565(127, 127, 127)
 
 #define TILE_SIZE 8
 
 extern int maps_map1[MAP_ROWS][MAP_COLS];
+extern int maps_map2[MAP_ROWS][MAP_COLS];
 
 static int (*map)[MAP_COLS] = maps_map1;
+
+struct character {
+	sdk_sprite_t s;
+	float speed;
+};
+
+static struct character player = {
+	.s = {
+		.ts = &ts_player_png,
+		.ox = 3.5f,
+		.oy = 3.5f,
+	},
+};
 
 void game_reset(void)
 {
@@ -22,11 +38,54 @@ void game_reset(void)
 void game_start(void)
 {
 	sdk_set_output_gain_db(6);
+
+	player.s.tile = 0;
+	player.s.x = TFT_WIDTH / 2.0f;
+	player.s.y = TFT_HEIGHT / 2.0f;
+	player.speed = 50.0f;
 }
 
 void game_audio(int nsamples)
 {
 	(void)nsamples;
+}
+
+void game_input(unsigned dt_usec)
+{
+	float dt = dt_usec / 1000000.0f;
+
+	if (sdk_inputs_delta.x > 0) {
+		map = maps_map1;
+	}
+
+	if (sdk_inputs_delta.y > 0) {
+		map = maps_map2;
+	}
+
+	float move_x = player.speed * sdk_inputs.joy_x / 2048.0f;
+	float move_y = player.speed * sdk_inputs.joy_y / 2048.0f;
+
+	if (fabsf(move_x) + fabs(move_y) > 0.1f) {
+		player.s.x += dt * move_x;
+		player.s.y += dt * move_y;
+
+		if (fabsf(move_x) > fabsf(move_y)) {
+			if (move_x > 0) {
+				player.s.tile = 6;
+			} else {
+				player.s.tile = 4;
+			}
+		} else {
+			if (move_y > 0) {
+				player.s.tile = 2;
+			} else {
+				player.s.tile = 0;
+			}
+		}
+
+		player.s.tile &= ~1;
+		player.s.tile |= (time_us_32() >> 16) & 1;
+	}
 }
 
 void game_paint(unsigned __unused dt_usec)
@@ -38,6 +97,8 @@ void game_paint(unsigned __unused dt_usec)
 			sdk_draw_tile(x * TILE_SIZE, y * TILE_SIZE, &ts_tileset_png, map[y][x]);
 		}
 	}
+
+	sdk_draw_sprite(&player.s);
 }
 
 int main()
