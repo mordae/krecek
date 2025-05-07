0ad6e175d5dd97da1b84393403b72ec580bfda76
Rafailek <rafaelmohl.rez@gmail.com>
2025-05-07 19:05:58 +0200

    touchtest: finish WIP version

--- src/touchtest/main.c
+++ src/touchtest/main.c
@@ -2,12 +2,16 @@
 #include <pico/stdlib.h>
 
 #include <stdint.h>
+#include <stdio.h>
 #include <tft.h>
 #include <sdk.h>
 #include <sdk/remote.h>
 
-static uint8_t position_x;
-static uint8_t position_y;
+static int position_x;
+static int position_y;
+static int position_z1;
+static int position_z2;
+static bool touching;
 
 void game_start(void)
 {
@@ -19,7 +23,6 @@ void game_start(void)
 
 static void exchange_bits(uint8_t *tx, uint8_t *rx)
 {
-    	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 0);
 	for (int i = 0; i < 8; i++) {
 		*rx <<= 1;
 
@@ -30,29 +33,70 @@ static void exchange_bits(uint8_t *tx, uint8_t *rx)
 
 		*tx <<= 1;
 	}
-	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 1);
+}
+
+static void get_12_bit_output(int *rx)
+{
+    	*rx = 0;
+	for (int i = 0; i < 12; i++) {
+    		*rx <<= 1;
+
+		remote_gpio_set(SLAVE_TOUCH_SCK_PIN, 1);
+		*rx |= remote_gpio_get(SLAVE_TOUCH_MISO_PIN);
+		remote_gpio_set(SLAVE_TOUCH_SCK_PIN, 0);
+	}
 }
 
 void game_input(unsigned __unused dt_usec)
 {
-	uint8_t control_byte_x = 0b11011011;
-	uint8_t control_byte_y = 0b10011011;
+	uint8_t control_byte_x =  0b10010011;
+	uint8_t control_byte_y =  0b11010011;
+	uint8_t control_byte_z1 = 0b10110011;
+	uint8_t control_byte_z2 = 0b11000011;
 	uint8_t null = 0;
 
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 0);
 	exchange_bits(&control_byte_x, &null);
-	// we may posibly need to wait here
-	exchange_bits(&null, &position_x);
+        get_12_bit_output(&position_x);
+        remote_gpio_set(SLAVE_TOUCH_CS_PIN, 1);
 
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 0);
 	exchange_bits(&control_byte_y, &null);
-	// we may posibly need to wait here
-	exchange_bits(&null, &position_y);
+	get_12_bit_output(&position_y);
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 1);
+
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 0);
+	exchange_bits(&control_byte_z1, &null);
+	get_12_bit_output(&position_z1);
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 1);
+
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 0);
+	exchange_bits(&control_byte_z2, &null);
+	get_12_bit_output(&position_z2);
+	remote_gpio_set(SLAVE_TOUCH_CS_PIN, 1);
+
+	if (position_z1 > 50) {
+    		touching = true;
+	} else {
+    		touching = false;
+	}
 }
 
 void game_paint(unsigned __unused dt_usec)
 {
 	tft_fill(0);
 
-	tft_draw_string(0, 0, rgb565(255, 255, 255), "x%i y%i\n", position_x, position_y);
+	char buf[50];
+
+	if (touching) {
+    		snprintf(buf, 50, "touching!");
+	} else {
+    		snprintf(buf, 50, "not touching!");
+	}
+
+	tft_draw_string(0, 0, rgb565(255, 255, 255), "x%5i y%5i\n", position_x, position_y);
+	tft_draw_string(0, 20, rgb565(255, 255, 255), "z1%5i z2%5i\n", position_z1, position_z2);
+	tft_draw_string(0, 40, rgb_to_rgb565(255, 255, 255), buf);
 }
 
 int main()
