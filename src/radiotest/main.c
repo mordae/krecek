52f9815b654f06797dbec0f4d95fff208436fa4c
Jan Hamal Dvořák <mordae@anilinux.org>
2025-05-19 00:07:08 +0200

    Make radiotest more interesting

--- src/radiotest/main.c
+++ src/radiotest/main.c
@@ -11,6 +11,8 @@
 
 #define WHITE rgb_to_rgb565(255, 255, 255)
 
+static int lx, ly, rx, ry;
+
 void game_start(void)
 {
 }
@@ -38,40 +40,49 @@ void game_inbox(sdk_message_t msg)
 	}
 
 	printf("\x1b[0m\n");
+
+	rx = msg.rf.data[0];
+	ry = msg.rf.data[1];
+}
+
+static void tx_cursor(void)
+{
+	int sizes[MAILBIN_RF_SLOTS];
+	uint32_t addrs[MAILBIN_RF_SLOTS];
+
+	remote_peek_many(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_size), (uint32_t *)sizes,
+			 MAILBIN_RF_SLOTS);
+
+	remote_peek_many(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_addr), addrs,
+			 MAILBIN_RF_SLOTS);
+
+	for (int i = 0; i < MAILBIN_RF_SLOTS; i++) {
+		if (sizes[i])
+			continue;
+
+		uint8_t buf[16];
+
+		buf[0] = 3;
+		buf[1] = 255;
+		buf[2] = lx;
+		buf[3] = ly;
+
+		remote_poke_many(addrs[i], (void *)buf, 1);
+		remote_poke(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_size) + i * 4, 4);
+
+		break;
+	}
 }
 
 void game_input(unsigned dt_usec)
 {
 	(void)dt_usec;
 
-	if (sdk_inputs.a > 0 || sdk_inputs_delta.y > 0) {
-		int sizes[MAILBIN_RF_SLOTS];
-		uint32_t addrs[MAILBIN_RF_SLOTS];
+	if (sdk_inputs.tp) {
+		lx = sdk_inputs.tx * TFT_RIGHT;
+		ly = sdk_inputs.ty * TFT_BOTTOM;
 
-		remote_peek_many(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_size),
-				 (uint32_t *)sizes, MAILBIN_RF_SLOTS);
-
-		remote_peek_many(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_addr), addrs,
-				 MAILBIN_RF_SLOTS);
-
-		for (int i = 0; i < MAILBIN_RF_SLOTS; i++) {
-			if (sizes[i])
-				continue;
-
-			static int counter = 0;
-
-			char buf[32];
-			int len = sprintf(buf + 2, "Hello, %i!", counter++);
-
-			buf[0] = len + 1;
-			buf[1] = 255;
-
-			remote_poke_many(addrs[i], (void *)buf, (len + 2 + 3) >> 2);
-			remote_poke(MAILBIN_BASE + offsetof(struct mailbin, rf_tx_size) + i * 4,
-				    len + 2);
-
-			break;
-		}
+		tx_cursor();
 	}
 }
 
@@ -80,6 +91,9 @@ void game_paint(unsigned dt_usec)
 	(void)dt_usec;
 
 	tft_fill(0);
+
+	tft_draw_rect(rx - 1, ry - 1, rx + 1, ry + 1, rgb_to_rgb565(255, 0, 0));
+	tft_draw_rect(lx - 1, ly - 1, lx + 1, ly + 1, rgb_to_rgb565(0, 255, 0));
 }
 
 int main()
