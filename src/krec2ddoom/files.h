dbb5febc82bb9e70a6ece24ff96bf69244db91a4
KOMHONZA323 <jan.honzik.jakoubek@gmail.com>
2025-10-22 07:23:26 +0200

    0.7 krecdoom editor fix

--- src/krec2ddoom/files.h
+++ src/krec2ddoom/files.h
@@ -4,12 +4,13 @@
 extern const TileType maps_map1[MAP_ROWS][MAP_COLS];
 extern const TileType maps_map2[MAP_ROWS][MAP_COLS];
 extern const TileType maps_map3[MAP_ROWS][MAP_COLS];
+extern const TileType maps_map4[MAP_ROWS][MAP_COLS];
 extern const TileType auto_safe[MAP_ROWS][MAP_COLS];
 
-#define FILE_NUM 4
+#define FILE_NUM 5
 
-static const TileType (*FILES[FILE_NUM])[MAP_COLS] = { auto_safe, maps_map1, maps_map2, maps_map3 };
+static const TileType (*FILES[FILE_NUM])[MAP_COLS] = { auto_safe, maps_map1, maps_map2, maps_map3, maps_map4 };
 
-static const char *FILES_NAME[FILE_NUM] = { "auto_safe", "maps_map1", "maps_map2", "maps_map3" };
+static const char *FILES_NAME[FILE_NUM] = { "auto_safe", "maps_map1", "maps_map2", "maps_map3", "maps_map4" };
 
-static const char *FILES_PATH[FILE_NUM] = { "safe", "map1", "map2", "map3" };
+static const char *FILES_PATH[FILE_NUM] = { "safe", "map1", "map2", "map3", "map4" };
--- src/krecdoom/include_maps.h
+++ src/krecdoom/include_maps.h
@@ -4,11 +4,13 @@
 extern const TileType maps_map1[MAP_ROWS][MAP_COLS];
 extern const TileType maps_map2[MAP_ROWS][MAP_COLS];
 extern const TileType maps_map3[MAP_ROWS][MAP_COLS];
+extern const TileType maps_map4[MAP_ROWS][MAP_COLS];
                                                                                                          
-#define FILE_NUM 3                                                                                       
+#define FILE_NUM 4                                                                                       
                                                                                                          
 static const TileType (*FILES[FILE_NUM])[MAP_COLS] = {                                                   
         maps_map1,
         maps_map2,
-        maps_map3
+        maps_map3,
+        maps_map4
 };
