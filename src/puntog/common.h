7ef2b6136a74f4a7663ed91b332640238b1fdf1f
KOMHONZA323 <jan.honzik.jakoubek@gmail.com>
2025-12-11 13:49:45 +0100

    Puntog update

--- src/puntog/common.h
+++ src/puntog/common.h
@@ -11,7 +11,31 @@
 #define PLAYER_WIDTH 6
 #define PLAYER_HEIGHT 12
 
-#define WHITE rgb_to_rgb565(255, 255, 255)
+#define AUTOSAFE_TIME 30 //seconds
+
+#define WHITE 0xFFFF
+#define RED 0xF800
+#define GREEN 0x07E0
+#define BLUE 0x001F
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define ORANGE 0xFD20
+#define PURPLE 0x8010
+#define PINK 0xFE19
+#define BROWN 0xA145
+#define GRAY 0x8410
+#define DARK_GRAY 0x4208
+#define LIGHT_GRAY 0xC618
+#define DARK_BLUE 0x0010
+#define DARK_GREEN 0x03E0
+#define DARK_RED 0x8000
+#define LIGHT_BLUE 0x051F
+#define LIGHT_GREEN 0x87F0
+#define LIGHT_RED 0xFC10
+
+#define DARK_GREY DARK_GRAY
+#define LIGHT_GREY LIGHT_GRAY
 
 typedef enum {
 	EMPTY = 0,
--- src/puntog/main.c
+++ src/puntog/main.c
@@ -1,14 +1,15 @@
 #include "common.h"
-#include <head-6x6.png.h>
-#include <lleg-2x2.png.h>
-#include <rleg-2x2.png.h>
 #include <body-4x4.png.h>
-#include <rarm-1x3.png.h>
+#include <head-6x6.png.h>
 #include <larm-1x3.png.h>
+#include <lleg-2x2.png.h>
+#include <rarm-1x3.png.h>
+#include <rleg-2x2.png.h>
 #include <sdk.h>
 #include <stdio.h>
 #include <string.h>
 #include <tiles-16x16.png.h>
+// string 7x10
 //*
 //-----TODO-----
 //
@@ -18,14 +19,41 @@
 //   Maps
 //
 //*
-
 extern TileType maps_tuturial[MAP_ROWS][MAP_COLS];
-
 TileType (*map)[MAP_COLS] = maps_tuturial;
+static const char keypad[4][4] = { { 'A', 'B', 'C', 'D' },
+				   { 'E', 'F', 'G', 'H' },
+				   { 'I', 'J', 'K', 'L' },
+				   { 'M', 'N', 'O', 'P' } };
+
+static const char keypad2[4][4] = { { 'Q', 'R', 'S', 'T' },
+				    { 'U', 'V', 'W', 'X' },
+				    { 'Y', 'Z', ' ', ' ' },
+				    { ' ', ' ', ' ', ' ' } };
+static int using_second = 0;
+static int cur_x = 0;
+static int cur_y = 0;
+static int name_len = 0;
+static char buffer[16] = { 0 };
+
+enum Game_Status {
+	COSTUME_MENU = 0,
+	NAME_MENU = 1,
+	IN_GAME,
+	MENU,
+	STATS_MENU,
+};
+
+enum Player_Status {
+	STANDING,
+	RUNING,
+};
 
 typedef struct Save {
 	int head, body, Larm, Rarm, Lleg, Rleg;
 	char name[16];
+	bool set;
+	float timespend;
 } Save;
 
 typedef struct {
@@ -35,34 +63,40 @@ typedef struct {
 	float speed;
 	int head, body, Larm, Rarm, Lleg, Rleg;
 	char name[16];
-	int ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10, ch11, ch12, ch13, ch14, ch15;
+	bool nameset;
+	int ch[15];
 	int direction, d2;
+	enum Player_Status S;
 } Player;
 
 typedef struct {
-	int num1, num2, num3, num4, num5, num6, num7, num8, num9, num0;
-	int ch;
-} key;
-typedef struct {
-	bool costume;
 	int whut;
 	int P_start_x;
 	int P_start_y;
-	bool start;
 	bool init;
+	float autosafe_timer;
+	float timespend;
+	int main_chose;
+	enum Game_Status S;
 } Menu;
 
 static Menu menu;
 static Player p;
-static key k;
 
 static void game_costume();
 static void game_read_player();
 static void game_save_player();
-static void game_start_menu();
 static void world_start();
 static void tiles();
 static void player_paint(float x, float y, int direction);
+static void draw_bordered_rect(int x1, int y1, int x2, int y2, color_t fill_color,
+			       color_t border_color);
+static void fill_rect(int x1, int y1, int x2, int y2, color_t color)
+{
+	for (int y = y1; y <= y2; y++) {
+		tft_draw_rect(x1, y, x2, y, color);
+	}
+}
 
 static void game_read_player()
 {
@@ -70,7 +104,13 @@ static void game_read_player()
 	sdk_save_read(0, &save, sizeof(save));
 
 	memcpy(p.name, save.name, sizeof(p.name));
-
+	if (isnan(save.timespend)) {
+		save.timespend = 0;
+		menu.timespend = 0;
+	} else {
+		menu.timespend = save.timespend;
+	}
+	p.nameset = save.set;
 	p.head = save.head;
 	p.body = save.body;
 	p.Larm = save.Larm;
@@ -82,6 +122,8 @@ static void game_read_player()
 static void game_save_player()
 {
 	Save save = {
+		.timespend = menu.timespend,
+		.set = 1,
 		.head = p.head,
 		.body = p.body,
 		.Larm = p.Larm,
@@ -91,192 +133,218 @@ static void game_save_player()
 	};
 
 	memcpy(save.name, p.name, sizeof(save.name));
-
 	sdk_save_write(0, &save, sizeof(save));
+}
+static void name_menu_input()
+{
+	if (sdk_inputs_delta.horizontal == -1 && cur_x > 0)
+		cur_x--;
+	if (sdk_inputs_delta.horizontal == 1 && cur_x < 3)
+		cur_x++;
+	if (sdk_inputs_delta.vertical == -1 && cur_y > 0)
+		cur_y--;
+	if (sdk_inputs_delta.vertical == 1 && cur_y < 3)
+		cur_y++;
+
+	if (sdk_inputs_delta.select == 1) {
+		using_second = !using_second;
+		cur_x = 0;
+		cur_y = 0;
+	}
+
+	char s = (using_second ? keypad2 : keypad)[cur_y][cur_x];
+
+	if (sdk_inputs_delta.a == 1 && name_len < 15) {
+		if ((s >= 'A' && s <= 'Z') || s == '-' || s == '_' || s == ' ') {
+			buffer[name_len++] = s;
+			buffer[name_len] = 0;
+			memcpy(p.name, buffer, 16);
+		}
+	}
+
+	if (sdk_inputs_delta.b == 1 && name_len > 0) {
+		buffer[--name_len] = 0;
+		memcpy(p.name, buffer, 16);
+	}
+
+	if (sdk_inputs_delta.start == 1 && name_len > 0) {
+		buffer[name_len] = 0;
+		memcpy(p.name, buffer, 16);
+		p.nameset = true;
+		menu.S = COSTUME_MENU;
+		game_save_player();
+	}
+}
+static void game_start_menu()
+{
+	tft_fill(0);
+
+	const int cell_w = 20;
+	const int cell_h = 15;
+	const int start_x = 20;
+	const int start_y = 15;
+
+	tft_draw_string(60, 2, YELLOW, "NAME");
+
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			int px = start_x + x * cell_w;
+			int py = start_y + y * cell_h;
+
+			char c = (using_second ? keypad2 : keypad)[y][x];
+
+			if (x == cur_x && y == cur_y) {
+				fill_rect(px, py, px + cell_w - 1, py + cell_h - 1, WHITE);
+				tft_draw_string(px + 6, py + 3, 0, "%c", c);
+			} else {
+				tft_draw_rect(px, py, px + cell_w - 1, py + cell_h - 1, WHITE);
+				tft_draw_string(px + 6, py + 3, WHITE, "%c", c);
+			}
+		}
+	}
+
+	//tft_draw_rect(10, 78, 150, 92, WHITE);
+	tft_draw_string(12, 80, WHITE, "%s", p.name);
+
+	int name_width = strlen(p.name) * 7;
+	if (name_width < 138) {
+		tft_draw_rect(15 + name_width, 83, 15 + name_width + 6, 89, CYAN);
+	}
+
+	tft_draw_string(10, 91, CYAN, "A-Set");
+	tft_draw_string(10, 101, CYAN, "B-Rem");
+	tft_draw_string(10, 112, CYAN, "START");
+
+	if (using_second) {
+		tft_draw_string(90, 93, GREEN, "PAGE 2");
+	} else {
+		tft_draw_string(90, 93, BLUE, "PAGE 1");
+	}
+
+	tft_draw_string(90, 105, YELLOW, "SEL-Page");
 }
 
 void game_start(void)
 {
-	k.ch = 1;
-	menu.start = true;
 	game_read_player();
+	if (p.nameset == 1) {
+		menu.S = IN_GAME;
+		menu.autosafe_timer = 0;
+		world_start();
+		return;
+	}
+
+	menu.S = NAME_MENU;
 }
+
+static void draw_bordered_rect(int x1, int y1, int x2, int y2, color_t fill_color,
+			       color_t border_color)
+{
+	fill_rect(x1, y1, x2, y2, fill_color);
+	tft_draw_rect(x1, y1, x2, y1, border_color);
+	tft_draw_rect(x1, y2, x2, y2, border_color);
+	tft_draw_rect(x1, y1, x1, y2, border_color);
+	tft_draw_rect(x2, y1, x2, y2, border_color);
+}
+
 static void world_start(void)
 {
 	p.x = 20;
 	p.y = 15;
 	p.speed = 50;
 	p.d2 = 0;
-
-	p.head = 0;
-	p.body = 0;
-	p.Larm = 0;
-	p.Rarm = 0;
-	p.Lleg = 0;
-	p.Lleg = 0;
 }
-static void zero_num()
-{
-	k.num0 = 0;
-	k.num1 = 0;
-	k.num2 = 0;
-	k.num3 = 0;
-	k.num4 = 0;
-	k.num5 = 0;
-	k.num6 = 0;
-	k.num7 = 0;
-	k.num8 = 0;
-	k.num9 = 0;
-}
-static void game_start_inputs()
-{
-	if (sdk_inputs_delta.a == 1) {
-		menu.P_start_y += 1;
-		zero_num();
-	}
-	if (sdk_inputs_delta.b == 1) {
-		menu.P_start_x += 1;
-		zero_num();
-	}
-	if (sdk_inputs_delta.x == 1) {
-		menu.P_start_x -= 1;
-		zero_num();
-	}
-	if (sdk_inputs_delta.y == 1) {
-		menu.P_start_y -= 1;
-		zero_num();
-	}
 
-	if (menu.P_start_x > 3 || menu.P_start_x < 0) {
-		menu.P_start_x = 0;
-	}
-
-	if (menu.P_start_y > 2 || menu.P_start_y < 0) {
-		menu.P_start_y = 0;
-	}
-	if (menu.P_start_x == 0 && menu.P_start_y == 0) {
-		menu.P_start_x = 1;
-	}
-	if (menu.P_start_x == 0 && menu.P_start_y == 2) {
-		menu.P_start_x = 1;
-	}
-	if (sdk_inputs_delta.start == 1) {
-		if (menu.P_start_x == 1 && menu.P_start_y == 0) {
-			k.num1 += 1;
-			if (k.num1 > 1)
-				k.num1 = 0;
-		}
-		if (menu.P_start_x == 2 && menu.P_start_y == 0) {
-			k.num2 += 1;
-			if (k.num2 > 3)
-				k.num2 = 0;
-		}
-		if (menu.P_start_x == 3 && menu.P_start_y == 0) {
-			k.num3 += 1;
-			if (k.num3 > 3)
-				k.num3 = 0;
-		}
-		if (menu.P_start_x == 0 && menu.P_start_y == 1) {
-			k.num0 += 1;
-			if (k.num0 > 1)
-				k.num0 = 0;
-		}
-		if (menu.P_start_x == 1 && menu.P_start_y == 1) {
-			k.num4 += 1;
-			if (k.num4 > 3)
-				k.num4 = 0;
-		}
-		if (menu.P_start_x == 2 && menu.P_start_y == 1) {
-			k.num5 += 1;
-			if (k.num5 > 3)
-				k.num5 = 0;
-		}
-		if (menu.P_start_x == 3 && menu.P_start_y == 1) {
-			k.num6 += 1;
-			if (k.num6 > 3)
-				k.num6 = 0;
-		}
-		if (menu.P_start_x == 1 && menu.P_start_y == 2) {
-			k.num7 += 1;
-			if (k.num7 > 3)
-				k.num7 = 0;
-		}
-		if (menu.P_start_x == 2 && menu.P_start_y == 2) {
-			k.num8 += 1;
-			if (k.num8 > 3)
-				k.num8 = 0;
-		}
-		if (menu.P_start_x == 3 && menu.P_start_y == 2) {
-			k.num9 += 1;
-			if (k.num9 > 3)
-				k.num9 = 0;
-		}
-	}
-	if (sdk_inputs_delta.select == 1) {
-		world_start();
-		menu.costume = true;
-		menu.start = false;
-	}
-
-	if (sdk_inputs_delta.vol_up == 1) {
-		k.ch += 1;
-		zero_num();
-	}
-	if (sdk_inputs_delta.vol_sw == 1) {
-		k.ch += 1;
-		zero_num();
-	}
-	if (sdk_inputs_delta.vol_down == 1) {
-		k.ch -= 1;
-		zero_num();
-	}
-	if (k.ch > 15) {
-		k.ch = 1;
-	}
-	if (k.ch < 1) {
-		k.ch = 15;
-	}
-	return;
-}
 void game_input(unsigned dt_usec)
 {
 	float dt = dt_usec / 1000000.0f;
-	if (menu.start) {
-		game_start_inputs();
+	menu.autosafe_timer += dt;
+	if (menu.autosafe_timer > AUTOSAFE_TIME) {
+		menu.timespend += menu.autosafe_timer;
+		game_save_player();
+		menu.autosafe_timer = 0;
 	}
 
-	if (sdk_inputs_delta.start == 1 && menu.costume == false) {
-		menu.costume = true;
+	switch (menu.S) {
+	case NAME_MENU:
+		name_menu_input();
 		return;
-	}
-	if (menu.costume) {
+		break;
+	case STATS_MENU:
+		if (sdk_inputs_delta.select == 1) {
+			menu.S = MENU;
+		}
+		break;
+	case COSTUME_MENU:
 		if (sdk_inputs_delta.start == 1) {
-			menu.costume = false;
+			menu.S = IN_GAME;
+			world_start();
 		}
-		if (sdk_inputs_delta.a == 1) {
-			menu.whut += 1;
-			if (menu.whut > 5)
-				menu.whut = 0;
+		if (sdk_inputs_delta.vertical == 1) {
+			menu.whut = (menu.whut + 1) % 6;
 		}
-		if (sdk_inputs_delta.y == 1) {
-			menu.whut -= 1;
-			if (menu.whut < 0)
-				menu.whut = 5;
+		if (sdk_inputs_delta.vertical == -1) {
+			menu.whut = (menu.whut - 1 + 6) % 6;
 		}
 		if (sdk_inputs_delta.b == 1) {
-			if (menu.whut == 0)
-				p.head += 1;
-			if (menu.whut == 1)
-				p.body += 1;
-			if (menu.whut == 2)
-				p.Rarm += 1;
-			if (menu.whut == 3)
-				p.Larm += 1;
-			if (menu.whut == 4)
-				p.Rleg += 1;
-			if (menu.whut == 5)
-				p.Lleg += 1;
+			switch (menu.whut) {
+			case 0:
+				p.head = (p.head + 1) % PLAYER_SPRITES;
+				break;
+			case 1:
+				p.body = (p.body + 1) % PLAYER_SPRITES;
+				break;
+			case 2:
+				p.Rarm = (p.Rarm + 1) % PLAYER_SPRITES;
+				break;
+			case 3:
+				p.Larm = (p.Larm + 1) % PLAYER_SPRITES;
+				break;
+			case 4:
+				p.Rleg = (p.Rleg + 1) % PLAYER_SPRITES;
+				break;
+			case 5:
+				p.Lleg = (p.Lleg + 1) % PLAYER_SPRITES;
+				break;
+			}
 		}
+		return;
+		break;
+
+	case MENU:
+		if (sdk_inputs_delta.vertical == -1 && menu.main_chose > 0) {
+			menu.main_chose--;
+		}
+		if (sdk_inputs_delta.vertical == 1 && menu.main_chose < 3) {
+			menu.main_chose++;
+		}
+		if (sdk_inputs_delta.start) {
+			switch (menu.main_chose) {
+			case 0:
+				menu.S = STATS_MENU;
+				break;
+			case 1:
+				menu.S = NAME_MENU;
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (sdk_inputs_delta.select == 1) {
+			menu.S = IN_GAME;
+		}
+		break;
+	default:
+		break;
+	}
+
+	if (sdk_inputs_delta.select == 1) {
+		menu.S = MENU;
 		return;
 	}
 
@@ -309,276 +377,100 @@ void game_paint(unsigned dt_usec)
 {
 	(void)dt_usec;
 	tft_fill(0);
-	if (menu.start) {
+
+	switch (menu.S) {
+	case NAME_MENU:
+		tft_set_origin(0, 0);
 		game_start_menu();
-		return;
-	}
-	if (menu.costume) {
+		break;
+	case COSTUME_MENU:
+		tft_set_origin(0, 0);
 		game_costume();
 		game_save_player();
-		return;
+		break;
+	case IN_GAME:
+		tft_set_origin(p.x - TFT_WIDTH / 2.0 + PLAYER_WIDTH / 2.0,
+			       p.y - TFT_HEIGHT / 2.0 + PLAYER_HEIGHT / 2.0);
+		tiles();
+		player_paint(p.x, p.y, p.direction);
+		break;
+	case MENU:
+		tft_set_origin(0, 0);
+		int y_pos = 50 + menu.main_chose * 15;
+		tft_draw_string(30, y_pos, YELLOW, ">");
+		tft_draw_string(65, 5, YELLOW, "MENU");
+
+		tft_draw_string(40, 50, WHITE, "STATS");
+		tft_draw_string(40, 65, WHITE, "NEW CHARACTER");
+		tft_draw_string(40, 65, WHITE, "");
+
+		break;
+	case STATS_MENU:
+		tft_set_origin(0, 0);
+		tft_draw_string(10, 10, YELLOW, "Time spend %-.2f H", menu.timespend / 60 / 60);
+		//printf("time spend %f\n", menu.timespend);
+		break;
+	default:
+		break;
 	}
-	tft_set_origin(p.x - TFT_WIDTH / 2.0 + PLAYER_WIDTH / 2.0,
-		       p.y - TFT_HEIGHT / 2.0 + PLAYER_HEIGHT / 2.0);
-	tiles();
-	player_paint(p.x, p.y, p.direction);
 }
 
 static void tiles()
 {
 	for (int y = 0; y < MAP_ROWS; y++) {
 		for (int x = 0; x < MAP_COLS; x++) {
-			if (map[y][x])
+			if (map[y][x]) {
 				sdk_draw_tile(x * TILE_SIZE, y * TILE_SIZE, &ts_tiles_16x16_png,
 					      map[y][x] - 1);
+			}
 		}
 	}
 }
-static void draw_rect_ins(int x1, int y1, int x2, int y2, color_t color)
-{
-	tft_draw_rect(x1, y1, x2, y1, color);
-	tft_draw_rect(x1, y2, x2, y2, color);
-	tft_draw_rect(x1, y1, x1, y2, color);
-	tft_draw_rect(x2, y1, x2, y2, color);
-}
 
-static void game_start_menu()
-{
-	int current_word = 0;
-
-	if (k.num2 > 0) {
-		if (k.num2 == 1) {
-			current_word = 65;
-		}
-		if (k.num2 == 2) {
-			current_word = 66;
-		}
-		if (k.num2 == 3) {
-			current_word = 67;
-		}
-	}
-	if (k.num3 > 0) {
-		if (k.num3 == 1) {
-			current_word = 68;
-		}
-		if (k.num3 == 2) {
-			current_word = 69;
-		}
-		if (k.num3 == 3) {
-			current_word = 70;
-		}
-	}
-	if (k.num4 > 0) {
-		if (k.num4 == 1) {
-			current_word = 71;
-		}
-		if (k.num4 == 2) {
-			current_word = 72;
-		}
-		if (k.num4 == 3) {
-			current_word = 73;
-		}
-	}
-	if (k.num5 > 0) {
-		if (k.num5 == 1) {
-			current_word = 74;
-		}
-		if (k.num5 == 2) {
-			current_word = 75;
-		}
-		if (k.num5 == 3) {
-			current_word = 76;
-		}
-	}
-	if (k.num6 > 0) {
-		if (k.num6 == 1) {
-			current_word = 77;
-		}
-		if (k.num6 == 2) {
-			current_word = 78;
-		}
-		if (k.num6 == 3) {
-			current_word = 79;
-		}
-	}
-	if (k.num7 > 0) {
-		if (k.num7 == 1) {
-			current_word = 81;
-		}
-		if (k.num7 == 2) {
-			current_word = 82;
-		}
-		if (k.num7 == 3) {
-			current_word = 83;
-		}
-	}
-	if (k.num8 > 0) {
-		if (k.num8 == 1) {
-			current_word = 84;
-		}
-		if (k.num8 == 2) {
-			current_word = 85;
-		}
-		if (k.num8 == 3) {
-			current_word = 86;
-		}
-	}
-	if (k.num9 > 0) {
-		if (k.num9 == 1) {
-			current_word = 87;
-		}
-		if (k.num9 == 2) {
-			current_word = 88;
-		}
-		if (k.num9 == 3) {
-			current_word = 89;
-		}
-	}
-	switch (k.ch) {
-	case 1:
-		p.ch1 = current_word;
-		break;
-	case 2:
-		p.ch2 = current_word;
-		break;
-	case 3:
-		p.ch3 = current_word;
-		break;
-	case 4:
-		p.ch4 = current_word;
-		break;
-	case 5:
-		p.ch5 = current_word;
-		break;
-	case 6:
-		p.ch6 = current_word;
-		break;
-	case 7:
-		p.ch7 = current_word;
-		break;
-	case 8:
-		p.ch8 = current_word;
-		break;
-	case 9:
-		p.ch9 = current_word;
-		break;
-	case 10:
-		p.ch10 = current_word;
-		break;
-	case 11:
-		p.ch11 = current_word;
-		break;
-	case 12:
-		p.ch12 = current_word;
-		break;
-	case 13:
-		p.ch13 = current_word;
-		break;
-	case 14:
-		p.ch14 = current_word;
-		break;
-	case 15:
-		p.ch15 = current_word;
-		break;
-	}
-
-	draw_rect_ins(menu.P_start_x * 40 - 2, menu.P_start_y * 30 + 10, menu.P_start_x * 40 + 7,
-		      menu.P_start_y * 30 + 23, WHITE);
-
-	char name[] = { p.ch1, p.ch2,  p.ch3,  p.ch4,  p.ch5,  p.ch6,  p.ch7,  p.ch8,
-			p.ch9, p.ch10, p.ch11, p.ch12, p.ch13, p.ch14, p.ch15, 0 };
-	strcpy(p.name, name);
-
-	tft_draw_string(70, 100, WHITE, "%s\n", p.name);
-
-	tft_draw_string(10, 100, WHITE, "-Start-");
-	tft_draw_string(40, 10, WHITE, "1");
-	tft_draw_string(80, 10, WHITE, "2");
-	tft_draw_string(120, 10, WHITE, "3");
-	tft_draw_string(40, 40, WHITE, "4");
-	tft_draw_string(80, 40, WHITE, "5");
-	tft_draw_string(120, 40, WHITE, "6");
-	tft_draw_string(40, 70, WHITE, "7");
-	tft_draw_string(80, 70, WHITE, "8");
-	tft_draw_string(120, 70, WHITE, "9");
-	tft_draw_string(0, 40, WHITE, "0");
-}
 static void player_paint(float x, float y, int direction)
 {
-	if (p.Lleg > PLAYER_SPRITES - 1)
-		p.Lleg = 0;
+	// Clamp values to valid range
+	p.head = (p.head + PLAYER_SPRITES) % PLAYER_SPRITES;
+	p.body = (p.body + PLAYER_SPRITES) % PLAYER_SPRITES;
+	p.Larm = (p.Larm + PLAYER_SPRITES) % PLAYER_SPRITES;
+	p.Rarm = (p.Rarm + PLAYER_SPRITES) % PLAYER_SPRITES;
+	p.Lleg = (p.Lleg + PLAYER_SPRITES) % PLAYER_SPRITES;
+	p.Rleg = (p.Rleg + PLAYER_SPRITES) % PLAYER_SPRITES;
 
-	if (p.Rleg > PLAYER_SPRITES - 1)
-		p.Rleg = 0;
-
-	if (p.body > PLAYER_SPRITES - 1)
-		p.body = 0;
-
-	if (p.head > PLAYER_SPRITES - 1)
-		p.head = 0;
-
-	if (p.Larm > PLAYER_SPRITES - 1)
-		p.Larm = 0;
-
-	if (p.Rarm > PLAYER_SPRITES - 1)
-		p.Rarm = 0;
-
-	if (p.Lleg < 0)
-		p.Lleg = PLAYER_SPRITES;
-
-	if (p.Rleg < 0)
-		p.Rleg = PLAYER_SPRITES;
-
-	if (p.body < 0)
-		p.body = PLAYER_SPRITES;
-
-	if (p.head < 0)
-		p.head = PLAYER_SPRITES;
-
-	if (p.Rarm < 0)
-		p.Rarm = PLAYER_SPRITES;
-
-	if (p.Larm < 0)
-		p.Larm = PLAYER_SPRITES;
-
-	// x and y its bottom center
-
-	// legs
+	// Draw player parts in correct order (back to front)
+	// Legs (behind body)
 	sdk_draw_tile(x - 1, y - 1, &ts_lleg_2x2_png, p.Lleg * PLAYER_DIRECTION + direction);
 	sdk_draw_tile(x + 1, y - 1, &ts_rleg_2x2_png, p.Rleg * PLAYER_DIRECTION + direction);
 
-	// body
+	// Body (covers top of legs)
 	sdk_draw_tile(x - 1, y - 5, &ts_body_4x4_png, p.body * PLAYER_DIRECTION + direction);
 
-	// Head
+	// Head (on top of body)
 	sdk_draw_tile(x - 2, y - 11, &ts_head_6x6_png, p.head * PLAYER_DIRECTION + direction);
 
-	// right arm
+	// Arms (on top of body)
 	sdk_draw_tile(x - 2, y - 5, &ts_rarm_1x3_png, p.Rarm * PLAYER_DIRECTION + direction);
-
-	// left arm
 	sdk_draw_tile(x + 3, y - 5, &ts_larm_1x3_png, p.Larm * PLAYER_DIRECTION + direction);
-
-	// pixel
-	//tft_draw_pixel(x, y, WHITE);
 }
+
 static void game_costume()
 {
 	tft_set_origin(0, 0);
+
+	draw_bordered_rect(100, 50, 170, 130, DARK_GRAY, WHITE);
 	player_paint(135, 90, p.d2);
 
-	// 120 , 8, 124 , 12
+	draw_bordered_rect(5, 5, 95, 175, DARK_BLUE, BLUE);
 
-	tft_draw_rect(100, 8 + menu.whut * 11, 104, 12 + menu.whut * 11, WHITE);
-	tft_draw_string(5, 05, WHITE, "Head      %i", p.head);
+	int y_pos = 10 + menu.whut * 15;
+	tft_draw_string(5, y_pos, YELLOW, ">");
 
-	tft_draw_string(5, 16, WHITE, "Body      %i", p.body);
-
-	tft_draw_string(5, 27, WHITE, "Right arm %i", p.Rarm);
-	tft_draw_string(5, 38, WHITE, "Left arm  %i", p.Larm);
-
-	tft_draw_string(5, 49, WHITE, "Right leg %i", p.Rleg);
-	tft_draw_string(5, 60, WHITE, "Left leg  %i", p.Lleg);
+	tft_draw_string(15, 10, WHITE, "Head  %i", p.head);
+	tft_draw_string(15, 25, WHITE, "Body  %i", p.body);
+	tft_draw_string(15, 40, WHITE, "R Arm %i", p.Rarm);
+	tft_draw_string(15, 55, WHITE, "L Arm %i", p.Larm);
+	tft_draw_string(15, 70, WHITE, "R Leg %i", p.Rleg);
+	tft_draw_string(15, 85, WHITE, "L Leg %i", p.Lleg);
 }
 
 int main()
